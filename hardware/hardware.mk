1e2b2a11bc3a732bf04ac7d197a1a75becf7118d
Joao D Lopes <joao.d.lopes91@gmail.com>
2021-11-24 15:10:18 +0000

    remove MODULE variable

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -1,9 +1,7 @@
-include $(LIB_DIR)/config.mk
-
 #add itself to MODULES list
-MODULES+=LIB
+MODULES+=$(shell make -C $(LIB_DIR) corename | grep -v make)
 
 #header
-INCLUDE+=$(incdir) $(LIB_DIR)/hardware/include
+INCLUDE+=$(incdir)$(LIB_DIR)/hardware/include
 #verilog sources
 $(foreach p, $(LIBVSRC), $(eval VSRC+=$p))
