77c66a2148c433d1be37bbd18f02ccd45ed88add
c <khilji@kth.se>
2021-12-16 15:41:33 +0500

    Added iob_lib.vh to VHDR in hardware.mk so that it appears in VHDR in asic skywater

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -3,5 +3,6 @@ MODULES+=$(shell make -C $(LIB_DIR) corename | grep -v make)
 
 #header
 INCLUDE+=$(incdir)$(LIB_DIR)/hardware/include
+VHDR+=$(LIB_DIR)/hardware/include/iob_lib.vh
 #verilog sources
 $(foreach p, $(LIBVSRC), $(eval VSRC+=$p))
