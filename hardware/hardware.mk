5adfacad0edebb094065fa1a7e811ec54214976f
Joao D Lopes <joao.d.lopes91@gmail.com>
2022-01-24 00:20:35 +0000

    add path to iob_lib.vh file to avoid duplicated headers; simplify hardware.mk

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -1,14 +1,10 @@
-define GET_LIB_MODULE_DIR
-$(shell find $(LIB_DIR) -name $(1))
-endef
-
 #lib header
-ifeq ($(filter iob_lib.vh, $(VHDR)),)
+ifeq ($(filter $(LIB_DIR)/hardware/include/iob_lib.vh, $(VHDR)),)
 INCLUDE+=$(incdir)$(LIB_DIR)/hardware/include
 VHDR+=$(LIB_DIR)/hardware/include/iob_lib.vh
 endif
 
 #include lib modules
-$(foreach p, $(LIB_MODULES), $(eval LIB_MODULES_DIRS+=$(call GET_LIB_MODULE_DIR, $p)))
+$(foreach p, $(LIB_MODULES), $(eval LIB_MODULES_DIRS+=$(shell find $(LIB_DIR) -name $p)))
 $(foreach p, $(LIB_MODULES_DIRS), $(eval include $p/hardware.mk))
 
