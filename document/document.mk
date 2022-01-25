f42cf156001b84f41002440025fe172b7948e5b4
P-Miranda <pmiranda.iobundle@gmail.com>
2022-01-25 19:03:16 +0000

    fix(DOC): Update documenation makefiles - Rename from TEX to LIB - Other minor fixes

--- document/document.mk
+++ document/document.mk
@@ -1,6 +1,7 @@
 #paths
-TEX:=$(TEX_DIR)/document
-TEX_SW_DIR:=$(TEX_DIR)/software
+LIB_DOC_DIR:=$(LIB_DIR)/document
+LIB_SW_DIR:=$(LIB_DIR)/software
+LIB_SW_PYTHON_DIR:=$(LIB_SW_DIR)/python
 
 #latex build macros
 BDTAB ?=1
@@ -10,7 +11,7 @@ SWCOMPS ?=0
 TD ?=0
 CUSTOM ?= 0
 
-TEX_DEFINES=\def\TEX{$(TEX)}\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\def\ASIC{$(ASIC)}
+TEX_DEFINES=\def\TEX{$(LIB_DOC_DIR)}\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\def\ASIC{$(ASIC)}
 TEX_DEFINES +=\def\SP{$(SP)}\def\SWREGS{$(SWREGS)}\def\SWCOMPS{$(SWCOMPS)}
 TEX_DEFINES +=\def\TD{$(TD)}\def\CUSTOM{$(CUSTOM)}
 
@@ -30,7 +31,7 @@ endif
 #add software accessible registers table to the list of tables
 ifeq ($(SWREGS),1)
 # expected sw_%reg table namming convention in CORENAMEsw_reg.v
-TAB +=$(shell grep START_TABLE $(I2S_TDM_DIR)/hardware/include/$(CORENAME)sw_reg.v | awk '{print $$2}' | sed s/$$/_tab.tex/)
+TAB +=$(shell grep START_TABLE $(CORE_DIR)/hardware/include/$(CORENAME)sw_reg.vh | awk '{print $$2}' | sed s/$$/_tab.tex/)
 endif 
 
 
@@ -41,15 +42,15 @@ all: figures fpga_res $(TAB) $(DOC).pdf
 pb.pdf: pb.aux
 	evince $@ &
 
-pb.aux: $(TEX)/pb/pb.tex $(SRC) $(TAB)
-	cp -u $(TEX)/pb/pb.cls .
+pb.aux: $(LIB_DOC_DIR)/pb/pb.tex $(SRC) $(TAB)
+	cp -u $(LIB_DOC_DIR)/pb/pb.cls .
 	pdflatex '$(TEX_DEFINES)\input{$<}'
 	pdflatex '$(TEX_DEFINES)\input{$<}'
 
 ug.pdf: ug.aux
 	evince $@ &
 
-ug.aux: $(TEX)/ug/ug.tex $(SRC) $(TAB) $(CORENAME)_version.txt
+ug.aux: $(LIB_DOC_DIR)/ug/ug.tex $(SRC) $(TAB) $(CORENAME)_version.txt
 	echo $(TAB)
 	exit
 	git rev-parse --short HEAD > shortHash.txt
@@ -72,7 +73,7 @@ presentation.aux: presentation.tex
 
 figures:
 	mkdir -p ./figures
-	cp -u $(TEX)/figures/* ./figures
+	cp -u $(LIB_DOC_DIR)/figures/* ./figures
 	cp -u ../figures/* ./figures
 	make -C ./figures
 
@@ -84,41 +85,41 @@ endif
 ifeq ($(INTEL),1)
 	cp $(CORE_DIR)/hardware/fpga/quartus/$(INT_FAMILY)/quartus.log .
 endif
-	INTEL=$(INTEL) XILINX=$(XILINX) $(TEX_SW_DIR)/fpga2tex.sh
+	INTEL=$(INTEL) XILINX=$(XILINX) $(LIB_SW_DIR)/fpga2tex.sh
 
 
 #block diagram
 bd_tab.tex: $(CORE_DIR)/hardware/src/$(BD_VSRC)
-	$(TEX_SW_DIR)/block2tex.py $@ $^
+	$(LIB_SW_PYTHON_DIR)/block2tex.py $@ $^
 
 #synthesis parameters
 sp_tab.tex: $(CORE_DIR)/hardware/src/$(TOP_MODULE).v
-	$(TEX_SW_DIR)/param2tex.py $< $@ $(CORE_DIR)/hardware/include/$(TOP_MODULE).vh
+	$(LIB_SW_PYTHON_DIR)/param2tex.py $< $@ $(CORE_DIR)/hardware/include/$(TOP_MODULE).vh
 
 #sw accessible registers
-sw_%reg_tab.tex: $(CORE_DIR)/hardware/include/$(CORENAME)sw_reg.v
-	$(TEX_SW_DIR)/swreg2tex.py $< 
+sw_%reg_tab.tex: $(CORE_DIR)/hardware/include/$(CORENAME)sw_reg.vh
+	$(LIB_SW_PYTHON_DIR)/swreg2tex.py $< 
 
 #general interface signals (clk and rst)
-gen_is_tab.tex: $(LIB_DIR)/hardware/include/gen_if.v
-	$(TEX_SW_DIR)/io2tex.py $< $@
+gen_is_tab.tex: $(LIB_DIR)/hardware/include/gen_if.vh
+	$(LIB_SW_PYTHON_DIR)/io2tex.py $< $@
 
-cpu_nat_s_is_tab.tex: $(LIB_DIR)/hardware/include/cpu_nat_s_if.v
-	$(TEX_SW_DIR)/io2tex.py $< $@
+cpu_nat_s_is_tab.tex: $(LIB_DIR)/hardware/include/cpu_nat_s_if.vh
+	$(LIB_SW_PYTHON_DIR)/io2tex.py $< $@
 
-cpu_nat_m_if.v: $(LIB_DIR)/hardware/include/cpu_nat_m_if.v
-	$(TEX_SW_DIR)/io2tex.py $< $@
+cpu_nat_m_if.vh: $(LIB_DIR)/hardware/include/cpu_nat_m_if.vh
+	$(LIB_SW_PYTHON_DIR)/io2tex.py $< $@
 
-cpu_axi4lite_s_is_tab.tex: $(LIB_DIR)/hardware/include/cpu_axi4lite_s_if.v
-	$(TEX_SW_DIR)/io2tex.py $< $@
+cpu_axi4lite_s_is_tab.tex: $(LIB_DIR)/hardware/include/cpu_axi4lite_s_if.vh
+	$(LIB_SW_PYTHON_DIR)/io2tex.py $< $@
 
-cpu_axi4_m_if_is_tab.tex:  $(LIB_DIR)/hardware/include/cpu_axi4_m_if.v
-	$(TEX_SW_DIR)/io2tex.py $< $@
+cpu_axi4_m_if_is_tab.tex:  $(LIB_DIR)/hardware/include/cpu_axi4_m_if.vh
+	$(LIB_SW_PYTHON_DIR)/io2tex.py $< $@
 
 #cleaning
 clean: ug-clean
 	@find . -type f -not \( $(NOCLEAN) \) -delete
 	@rm -rf figures
-	@rm -rf $(TEX_SW_DIR)/__pycache__/ $(TEX_SW_DIR)/*.pyc
+	@rm -rf $(LIB_SW_PYTHON_DIR)/__pycache__/ $(LIB_SW_PYTHON_DIR)/*.pyc
 
 .PHONY:  all figures fpga_res ug-clean clean
