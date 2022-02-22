370b49e02950bca479722fea07a7653470e5d2fd
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-02-22 23:45:23 +0000

    do not use macros

--- hardware/sync/sync.v
+++ hardware/sync/sync.v
@@ -1,5 +1,4 @@
 `timescale 1ns / 1ps
-`include "iob_lib.vh"
 
 module sync
   #(
@@ -7,14 +6,22 @@ module sync
     parameter RST_VAL = 0
     )
   (
-   `INPUT(clk, 1),
-   `INPUT(rst, 1),
-   `INPUT(signal_in, WIDTH),
-   `OUTPUT_VAR(signal_out, WIDTH)
+   input clk,
+   input rst,
+   input [WIDTH-1:0] signal_in,
+   output reg [WIDTH-1:0] signal_out
    );
+   
 
-   `VAR(sync_reg, WIDTH)
-   `REG_AR(clk, rst, 1'b0, sync_reg, signal_in)
-   `REG_AR(clk, rst, 1'b0, signal_out, sync_reg)
+   reg [WIDTH-1:0]        sync_reg;
+
+   always @(posedge clk, posedge rst)
+     if(rst) begin
+        sync_reg <= RST_VAL;
+        signal_out <= RST_VAL;
+     end else begin
+        sync_reg <= signal_in;
+        signal_out <= sync_reg;
+     end
 
 endmodule
