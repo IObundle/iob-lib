49ad7e79b41efdae670a95e7fc771414235df9ed
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-02-23 00:49:59 +0000

    add label

--- hardware/sync/sync.v
+++ hardware/sync/sync.v
@@ -15,13 +15,14 @@ module sync
 
    reg [WIDTH-1:0]        sync_reg;
 
-   always @(posedge clk, posedge rst)
-     if(rst) begin
-        sync_reg <= RST_VAL;
-        signal_out <= RST_VAL;
-     end else begin
-        sync_reg <= signal_in;
-        signal_out <= sync_reg;
-     end
-
+   always @(posedge clk, posedge rst) begin : sync
+      if(rst) begin
+         sync_reg <= RST_VAL;
+         signal_out <= RST_VAL;
+      end else begin
+         sync_reg <= signal_in;
+         signal_out <= sync_reg;
+      end
+   end
+   
 endmodule
