501768822e234df92edad6d5a7f315740aa926f2
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-02-24 15:49:10 +0000

    improve document generation

--- document/document.mk
+++ document/document.mk
@@ -5,6 +5,10 @@ LIB_SW_DIR:=$(LIB_DIR)/software
 LIB_SW_PYTHON_DIR:=$(LIB_SW_DIR)/python
 
 $(DOC).pdf: fpga_res figures
+	echo "\def\TEX{$(LIB_DOC_DIR)}" > $(DOC)top.tex
+	if [ -f vivado.tex ]; then XILINX=1; else XILINX=0; fi; echo "\def\XILINX{$$XILINX}" >> $(DOC)top.tex
+	if [ -f quartus.tex ]; then INTEL=1; else INTEL=0; fi; echo "\def\INTEL{$$INTEL}" >> $(DOC)top.tex
+	if [ -f asic.tex ]; then ASIC=1; else ASIC=0; fi; echo "\def\ASIC{$$ASIC}" >> $(DOC)top.tex
 ifeq ($(DOC),pb)
 	make -C ./figures pb_figs
 endif
@@ -12,10 +16,12 @@ ifeq ($(DOC),ug)
 	make -C ./figures ug_figs
 	echo $(VERSION) > version.tex
 	git rev-parse --short HEAD > shortHash.tex
-	make ugtop.tex
+	make $(DOC)top.tex
+endif
+	echo "\input{$(LIB_DOC_DIR)/$(DOC)/$(DOC).tex}" >> $(DOC)top.tex
 	pdflatex -jobname $(DOC) $(DOC)top.tex
 	if [ -f *.bib ]; then bibtex ug; fi
-endif
+	make $(DOC)top.tex
 	pdflatex -jobname $(DOC) $(DOC)top.tex
 	pdflatex -jobname $(DOC) $(DOC)top.tex
 
@@ -24,21 +30,13 @@ endif
 view: $(DOC).pdf
 	evince $< &
 
-pbtop.tex: figures
-	cp $(LIB_DOC_DIR)/pb/pb.tex
-
 ugtop.tex: texfiles
-	echo "\def\TEX{$(LIB_DOC_DIR)}" > $@
 	if [ -f sp_tab.tex ]; then SP=1; else SP=0; fi; echo "\def\SP{$$SP}" >> $@
 	if [ -f sm_tab.tex ]; then SM=1; else SM=0; fi; echo "\def\SM{$$SM}" >> $@
 	if [ `find . -name \*swreg_tab.tex -print -quit` ]; then SWREGS=1; else SWREGS=0; fi; echo "\def\SWREGS{$$SWREGS}" >> $@
 	if [ -f sw_tab.tex ]; then SWCOMPS=1; else SWCOMPS=0; fi; echo "\def\SWCOMPS{$$SWCOMPS}" >> $@
 	if [ -f custom.tex ]; then CUSTOM=1; else CUSTOM=0; fi; echo "\def\CUSTOM{$$CUSTOM}" >> $@
-	if [ -f vivado.tex ]; then XILINX=1; else XILINX=0; fi; echo "\def\XILINX{$$XILINX}" >> $@
-	if [ -f quartus.tex ]; then INTEL=1; else INTEL=0; fi; echo "\def\INTEL{$$INTEL}" >> $@
-	if [ -f asic.tex ]; then ASIC=1; else ASIC=0; fi; echo "\def\ASIC{$$ASIC}" >> $@
 	if [ `find figures -name *td_fig.pdf -print -quit` ]; then TD=1; else TD=0; fi; echo "\def\TD{$$TD}" >> $@
-	echo "\input{$(LIB_DOC_DIR)/ug/$(DOC).tex}" >> $@
 
 #tex files extracted from code comments
 texfiles: $(MACRO_LIST)
