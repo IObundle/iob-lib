4e5c78db51dd33f74179f3661f8fa7732caddeb6
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-04-03 00:18:41 +0100

    remove unused functions

--- software/include/iob-lib.h
+++ software/include/iob-lib.h
@@ -1,18 +1,6 @@
 #ifndef PC
-//memory access macros
-#define MEM_SET(type, location, value) (*((type*) (location)) = (value) )
-#define MEM_GET(type, location)        (*((type*) (location)))
-
 //stream access macros
 #define IO_SET(base, location, value) (*((volatile int*) ( (base) + (sizeof(int)) * (location) )) = (value) )
 #define IO_GET(base, location)        (*((volatile int*) ( (base) + (sizeof(int)) * (location) )))
-#else // ifdef PC
-//memory access functions
-void MEM_SET(int type, int location, int value);
-int MEM_GET(int type, int location);
-
-//stream access functions
-void IO_SET(int base, int location, int value);
-int IO_GET(int base, int location);
 #endif
 
