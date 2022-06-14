78028456822d0b4faa563c44d2cdc886b4667fca
behzad joudat <joudat.behzad@gmail.com>
2022-06-14 11:34:53 +0100

    changes added to version.py to creat a .vh file and input a line in it

--- software/python/version.py
+++ software/python/version.py
@@ -2,30 +2,44 @@
 
 import sys
 
-if len(sys.argv) < 2 or len(sys.argv) > 2:
-    print "You must set one argument!!!"
+if len(sys.argv) < 3 or len(sys.argv) > 3:
+    print ("You must set two argument!!!")
     sys.exit()
 
-
-def nostring(s):
+def numbers(s):
     try:
         int(s)
         return True
     except ValueError:
         return False
 
-for arg in sys.argv[1]:
-    if not nostring(arg):
-        sys.exit("All arguments must be integers. Exit.")
+for arg in sys.argv[2]:
+    if not numbers(arg):
+        sys.exit("second argument must be integers. Exit.")
 
-a = sys.argv[1]
+for arg in sys.argv[1]:
+    if numbers(arg):
+        sys.exit("the first argument should not be numbers!")
+
+text= sys.argv[1]
+        
+a = sys.argv[2]
 
 fh = int(a[:2])
 
 sh = int(a[2:])
 
-print ("V{}.{}".format(fh,sh))
+thename = ("{}_V{}.{}".format(text,fh,sh))
 
+print (thename)
 
+#the path should be indicated
+the_path = "./software/python/"
 
+thecompletefile = ("{}{}.vh".format(the_path,thename))
 
+f = open(thecompletefile, "w+")
+
+f.write("`define VERSION {}".format(int(a)))
+
+f.close()
