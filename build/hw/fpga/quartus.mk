f1b48994b8fc41c96a21157f4712c56618043128
codingUniv <salisaf185@gmail.com>
2022-07-11 11:42:58 +0100

    update quartus.mk; update VERILOG_AXI submodule

--- build/hw/fpga/quartus.mk
+++ build/hw/fpga/quartus.mk
@@ -10,6 +10,7 @@ $(FPGA_OBJ): $(VHDR) $(VSRC) $(wildcard *.sdc)
 	$(ENV) quartus_sh -t quartus.tcl $(TOP_MODULE) $(DEFINE) "$(VSRC)" $(FPGA_PART) $(NAME)
 	$(ENV) quartus_map --read_settings_files=on --write_settings_files=off $(TOP_MODULE) -c $(TOP_MODULE)
 	$(ENV) quartus_fit --read_settings_files=off --write_settings_files=off $(TOP_MODULE) -c $(TOP_MODULE)
+	$(ENV) quartus_sta $(TOP_MODULE) -c $(TOP_MODULE) --do_report_timing
 	$(ENV) quartus_cdb --read_settings_files=off --write_settings_files=off $(TOP_MODULE) -c $(TOP_MODULE) --merge=on
 	$(ENV) quartus_cdb $(TOP_MODULE) -c $(TOP_MODULE) --incremental_compilation_export=$(TOP_MODULE)_0.qxp --incremental_compilation_export_partition_name=Top --incremental_compilation_export_post_synth=on --incremental_compilation_export_post_fit=off --incremental_compilation_export_routing=on --incremental_compilation_export_flatten=on
 	mv output_files/*.fit.summary $(FPGA_LOG)
