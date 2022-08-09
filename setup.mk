16090acde1bc711e138a1e377345ac32bcbfbdea
PMiranda <pmiranda.iobundle@gmail.com>
2022-08-09 18:05:47 +0100

    fix(subcore): targets to setup and clean subcores - add targets to setup and clean sub cores - if a CORE has other cores as submodules, run `make -C $(SUBCORE_DIR)   setup` for each sub core before the top core's setup. - do the same for the clean procedure

--- setup.mk
+++ setup.mk
@@ -40,8 +40,15 @@ BUILD_PPROC_SW_PC_DIR:=$(BUILD_PPROC_DIR)/sw/pc
 MKREGS:=build/sw/python/mkregs.py
 
 # create build directory
-$(BUILD_DIR):
+$(BUILD_DIR): setup-sub-cores
 	cp -r -u build $@
+
+# setup sub cores
+# sub core paths
+SUB_CORE_DIRS:=$(shell find $(CORE_DIR)/submodules/ -name info.mk -printf '%h\n')
+
+setup-sub-cores:
+	$(foreach subcore,$(SUB_CORE_DIRS),$(shell make -C $(subcore) setup))
 
 # import core hardware and simulation files
 include $(CORE_DIR)/hardware/hardware.mk
