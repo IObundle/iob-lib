8a0227213aede62eba6e06124d2816c238e998b2
PMiranda <pmiranda.iobundle@gmail.com>
2022-08-09 12:30:11 +0100

    feat(sw): build core drivers as static library - add support to copy CORE's software sources and headers to   `BUILD_DIR/sw/src` - compile and create static library for embedded and pc-emul versions of   CORE's software drivers into `BUILD_DIR/pproc/sw/emb` and   `BUILD_DIR/pproc/sw/pc`.

--- setup.mk
+++ setup.mk
@@ -20,6 +20,10 @@ VERSION_STR:=$(shell software/python/version.py $(NAME) $(VERSION))
 
 # establish build dir paths
 BUILD_DIR := $(CORE_DIR)/$(NAME)_$(VERSION_STR)
+BUILD_SW_DIR:=$(BUILD_DIR)/sw
+BUILD_SW_SRC_DIR:=$(BUILD_DIR)/sw/src
+BUILD_SW_PC_DIR:=$(BUILD_SW_DIR)/pc
+BUILD_SW_EMB_DIR:=$(BUILD_SW_DIR)/emb
 BUILD_VSRC_DIR:=$(BUILD_DIR)/hw/vsrc
 BUILD_SIM_DIR:=$(BUILD_DIR)/hw/sim
 BUILD_FPGA_DIR:=$(BUILD_DIR)/hw/fpga
@@ -29,6 +33,8 @@ BUILD_FIG_DIR:=$(BUILD_DOC_DIR)/figures
 BUILD_SYN_DIR:=$(BUILD_DIR)/hw/syn
 BUILD_PPROC_DIR:=$(BUILD_DIR)/pproc
 BUILD_PPROC_HW_DIR:=$(BUILD_PPROC_DIR)/hw
+BUILD_PPROC_SW_EMB_DIR:=$(BUILD_PPROC_DIR)/sw/emb
+BUILD_PPROC_SW_PC_DIR:=$(BUILD_PPROC_DIR)/sw/pc
 
 # mkregs path
 MKREGS:=build/sw/python/mkregs.py
@@ -41,6 +47,9 @@ $(BUILD_DIR):
 include $(CORE_DIR)/hardware/hardware.mk
 include $(CORE_DIR)/hardware/simulation/sim_setup.mk
 
+# import core software files
+include $(CORE_DIR)/software/software.mk
+
 # copy core version header file
 VHDR+=$(BUILD_VSRC_DIR)/$(NAME)_version.vh
 $(BUILD_VSRC_DIR)/$(NAME)_version.vh: $(NAME)_version.vh
@@ -49,7 +58,7 @@ $(BUILD_VSRC_DIR)/$(NAME)_version.vh: $(NAME)_version.vh
 # tex files requiring specific processing
 tex_files:=deliverables config benefits results fpga_results asic_results features intro
 
-setup: $(BUILD_DIR) $(VHDR) $(VSRC)
+setup: $(BUILD_DIR) $(VHDR) $(VSRC) $(HDR) $(SRC)
 	echo "VERSION_STR=$(VERSION_STR)" > $(BUILD_DIR)/version.mk
 	cp -u $(CORE_DIR)/info.mk $(BUILD_DIR)
 ifneq ($(wildcard $(CORE_DIR)/mkregs.conf),)
@@ -88,6 +97,12 @@ ifneq ($(SETUP_PPROC),0)
 	cp -u $(VHDR) $(BUILD_PPROC_HW_DIR)
 	cp -u $(VSRC) $(BUILD_PPROC_HW_DIR)
 	make -C $(BUILD_PPROC_HW_DIR) build
+	cp -u $(SW_EMB_SRC) $(BUILD_PPROC_SW_EMB_DIR)
+	cp -u $(SW_EMB_HDR) $(BUILD_PPROC_SW_EMB_DIR)
+	make -C $(BUILD_PPROC_SW_EMB_DIR) build
+	cp -u $(SW_PC_SRC) $(BUILD_PPROC_SW_PC_DIR)
+	cp -u $(SW_PC_HDR) $(BUILD_PPROC_SW_PC_DIR)
+	make -C $(BUILD_PPROC_SW_PC_DIR) build
 endif
 
 .PHONY: version setup
