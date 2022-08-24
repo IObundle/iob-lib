e2a44908d002a6160efeacb3a4dda63425a31d66
Joao D Lopes <joao.d.lopes91@gmail.com>
2022-08-24 19:18:16 +0100

    fix iob_reset_sync module; simplify hardware.mk files

--- hardware/iob_reset_sync/hardware.mk
+++ hardware/iob_reset_sync/hardware.mk
@@ -1,5 +1,4 @@
-
-ifneq (iob_reset_sync,$(filter iob_reset_sync, $(HW_MODULES)),)
+ifeq ($(filter iob_reset_sync, $(HW_MODULES)),)
 
 # Add to modules list
 HW_MODULES+=iob_reset_sync
--- hardware/iob_reset_sync/iob_reset_sync.v
+++ hardware/iob_reset_sync/iob_reset_sync.v
@@ -1,20 +1,20 @@
 `timescale 1ns / 1ps
 
-`include "iob_lib.vh"
-
 module iob_reset_sync
   (
-   `IOB_INPUT(clk, 1),
-   `IOB_INPUT(rst, 1),
-   `IOB_OUTPUT(rst_out, 1)
+   input  clk,
+   input  rst,
+   output rst_out
    );
 
    reg [1:0] sync_reg;
-   always @(posedge clk, posedge rst)
-     if (rst)
-       sync_reg <= 2'b11;
-     else
-       sync_reg <= {sync_reg[0], 1'b0};
+   always @(posedge clk, posedge rst) begin
+      if (rst) begin
+         sync_reg <= 2'b11;
+      end else begin
+         sync_reg <= {sync_reg[0], 1'b0};
+      end
+   end
 
    assign rst_out = sync_reg[1];
 
