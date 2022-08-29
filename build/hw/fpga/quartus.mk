5b7adbf03f5282745b6d00d4e38361a37bb9159a
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-08-29 20:16:56 +0100

    move quartus flow entirely into .tcl

--- build/hw/fpga/quartus.mk
+++ build/hw/fpga/quartus.mk
@@ -1,16 +1,10 @@
-FPGA_OBJ:=$(TOP_MODULE)_0.qxp
+FPGA_OBJ:=$(NAME).qxp
 FPGA_TEX:=quartus.tex
 FPGA_SERVER=$(QUARTUS_SERVER)
 FPGA_USER=$(QUARTUS_USER)
 FPGA_ENV=$(QUARTUSPATH)/nios2eds/nios2_command_shell.sh
 
-#map, fit, sta and cbd should be moved into quartus.tcl and removed from here
 $(FPGA_OBJ): $(VHDR) $(VSRC) $(wildcard *.sdc)
 	$(FPGA_ENV) quartus_sh -t quartus.tcl $(NAME) $(TOP_MODULE) "$(VSRC)" $(FPGA_PART)
-	$(FPGA_ENV) quartus_map --read_settings_files=on --write_settings_files=off $(TOP_MODULE) -c $(TOP_MODULE)
-	$(FPGA_ENV) quartus_fit --read_settings_files=off --write_settings_files=off $(TOP_MODULE) -c $(TOP_MODULE)
-	$(FPGA_ENV) quartus_sta $(TOP_MODULE) -c $(TOP_MODULE) --do_report_timing
-	$(FPGA_ENV) quartus_cdb --read_settings_files=off --write_settings_files=off $(TOP_MODULE) -c $(TOP_MODULE) --merge=on
-	$(FPGA_ENV) quartus_cdb $(TOP_MODULE) -c $(TOP_MODULE) --incremental_compilation_export=$(TOP_MODULE)_0.qxp --incremental_compilation_export_partition_name=Top --incremental_compilation_export_post_synth=on --incremental_compilation_export_post_fit=off --incremental_compilation_export_routing=on --incremental_compilation_export_flatten=on
 	LOG=output_files/*.fit.summary ../../sw/quartus2tex.sh
 
--- build/hw/fpga/quartus.tcl
+++ build/hw/fpga/quartus.tcl
@@ -4,17 +4,30 @@ set TOP [lindex $argv 1]
 set VSRC [lindex $argv 2]
 set PART [lindex $argv 3]
 
-set QUARTUS_VERSION "18.0.0 Standard Edition"
+set project_name $NAME
+
 set FAMILY "Cyclone V"
 
-set INST [concat $TOP "_0"]
-
 project_new $TOP -overwrite
+#------ Create or open project ------#
+if [project_exists $project_name] {
 
+    #------ Project already exists -- open project -------#
+    project_open $project_name -force
+} else {
+
+    #------ Project does not exist -- create new project ------#
+    project_new $project_name
+}
+
+set_global_assignment -name TOP_LEVEL_ENTITY $TOP
+
+#device info
 set_global_assignment -name FAMILY $FAMILY
 set_global_assignment -name DEVICE $PART
+
 set_global_assignment -name PROJECT_OUTPUT_DIRECTORY output_files
-set_global_assignment -name TOP_LEVEL_ENTITY $TOP
+
 set_global_assignment -name VERILOG_INPUT_VERSION SYSTEMVERILOG_2005
 
 #verilog heders search path
@@ -33,19 +46,47 @@ set_global_assignment -name PARTITION_FITTER_PRESERVATION_LEVEL PLACEMENT_AND_RO
 set_global_assignment -name PARTITION_COLOR 16764057 -section_id Top
 
 
-set_global_assignment -name PARTITION_NETLIST_TYPE POST_SYNTH -section_id $TOP:$INST
+set_global_assignment -name PARTITION_NETLIST_TYPE POST_SYNTH -section_id $TOP:$TOP
 
-set_global_assignment -name PARTITION_FITTER_PRESERVATION_LEVEL PLACEMENT_AND_ROUTING -section_id $TOP:$INST
+set_global_assignment -name PARTITION_FITTER_PRESERVATION_LEVEL PLACEMENT_AND_ROUTING -section_id $TOP:$TOP
 
-set_global_assignment -name PARTITION_COLOR 39423 -section_id $TOP:$INST
+set_global_assignment -name PARTITION_COLOR 39423 -section_id $TOP:$TOP
 
 set_instance_assignment -name PARTITION_HIERARCHY root_partition -to | -section_id Top
 
-set_global_assignment -name LAST_QUARTUS_VERSION $QUARTUS_VERSION
+set_global_assignment -name LAST_QUARTUS_VERSION "18.0.0 Standard Edition"
 set_global_assignment -name SDC_FILE ../fpga/$NAME.sdc
 set_global_assignment -name MIN_CORE_JUNCTION_TEMP 0
 set_global_assignment -name MAX_CORE_JUNCTION_TEMP 85
 set_global_assignment -name POWER_PRESET_COOLING_SOLUTION "23 MM HEAT SINK WITH 200 LFPM AIRFLOW"
 set_global_assignment -name POWER_BOARD_THERMAL_MODEL "NONE (CONSERVATIVE)"
+
+#------ Compile using ::quartus::flow ------#
+
+set_global_assignment -name SEED 23
+
+#------ Manually recompile and perform timing analysis again using qexec ------#
+
+# Write these assignments to the
+# Quartus Prime Settings File (.qsf) so that
+# the Quartus Prime command-line executables
+# can use these assignments during compilation
+export_assignments
+
+if [catch {qexec "[file join $::quartus(binpath) quartus_map] $project_name"} result] {
+    qexit -error
+}
+
+if [catch {qexec "[file join $::quartus(binpath) quartus_fit] $project_name"} result] {
+    qexit -error
+}
+
+if [catch {qexec "[file join $::quartus(binpath) quartus_sta] $project_name"} result] {
+    qexit -error
+}
+
+if [catch {qexec "[file join $::quartus(binpath) quartus_cdb] $project_name --incremental_compilation_export=$NAME.qxp --incremental_compilation_export_post_synth=on"} result] {
+    qexit -error
+}
 
 project_close
