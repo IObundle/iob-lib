96f1c084af0fcbf9577d74d0a0718cb564cc9b80
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-09-02 16:06:28 +0100

    remove pycache

--- setup.mk
+++ setup.mk
@@ -133,6 +133,7 @@ endif
 clean:
 	@if [ -f $(BUILD_DIR)/Makefile ]; then make -C $(BUILD_DIR) clean; fi
 	@rm -rf $(BUILD_DIR)
+	@rm -rf software/python/__pycache__
 
 debug: $(BUILD_DIR) $(VHDR) 
 	@echo $(NAME)
