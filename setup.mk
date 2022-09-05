d1ae83d7894107ba556261dbc49cccdc418f0385
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-09-05 23:16:15 +0100

    improve sim setup and build, remove unused script

--- setup.mk
+++ setup.mk
@@ -7,10 +7,9 @@ SHELL=/bin/bash
 export
 
 LIB_DIR=submodules/LIB
-CORE_DIR =.
 
-include $(CORE_DIR)/info.mk
-include $(CORE_DIR)/config_setup.mk
+include info.mk
+include config_setup.mk
 
 # lib paths
 LIB_PYTHON_DIR=software/python
@@ -23,20 +22,20 @@ SETUP_FPGA ?=1
 SETUP_DOC ?=1
 
 # core internal paths
-CORE_SW_DIR=$(CORE_DIR)/software
-CORE_EMB_DIR=$(CORE_SW_DIR)/embedded
-CORE_PC_DIR=$(CORE_SW_DIR)/pc-emul
+SW_DIR=software
+EMB_DIR=$(SW_DIR)/embedded
+PC_DIR=$(SW_DIR)/pc-emul
 
-CORE_HW_DIR=$(CORE_DIR)/hardware
-CORE_SIM_DIR=$(CORE_HW_DIR)/simulation
-CORE_FPGA_DIR=$(CORE_HW_DIR)/fpga
-CORE_DOC_DIR=$(CORE_DIR)/document
+HW_DIR=hardware
+SIM_DIR=$(HW_DIR)/simulation
+FPGA_DIR=$(HW_DIR)/fpga
+DOC_DIR=document
 
 
 # establish build dir paths
-VERSION_STR := $(shell $(LIB_DIR)/software/python/version.py -i $(CORE_DIR))
+VERSION_STR := $(shell $(LIB_DIR)/software/python/version.py -i .)
 
-BUILD_DIR := $(CORE_DIR)/$(NAME)_$(VERSION_STR)
+BUILD_DIR := $(NAME)_$(VERSION_STR)
 BUILD_SW_DIR:=$(BUILD_DIR)/sw
 BUILD_SW_PYTHON_DIR:=$(BUILD_SW_DIR)/python
 BUILD_SW_SRC_DIR:=$(BUILD_DIR)/sw/src
@@ -69,70 +68,73 @@ endif
 ifeq ($(SETUP_DOC),0)
 	rm -rf $(BUILD_DOC_DIR)
 else
-ifneq ($(wildcard $(CORE_DIR)/mkregs.conf),)
-	cp -u $(CORE_DIR)/mkregs.conf $(BUILD_TSRC_DIR)
+ifneq ($(wildcard mkregs.conf),)
+	cp -u mkregs.conf $(BUILD_TSRC_DIR)
 endif
 endif
-	cp -u $(CORE_DIR)/info.mk $(BUILD_DIR)
-	cp -u $(CORE_DIR)/config_setup.mk $(BUILD_DIR)
-ifneq ($(wildcard $(CORE_DIR)/config.mk),)
-	cp -u $(CORE_DIR)/config.mk $(BUILD_DIR)
+	cp -u info.mk $(BUILD_DIR)
+	cp -u config_setup.mk $(BUILD_DIR)
+ifneq ($(wildcard config.mk),)
+	cp -u config.mk $(BUILD_DIR)
 endif
 
 # import core hardware and simulation files
-include $(CORE_HW_DIR)/hardware.mk
-include $(CORE_SIM_DIR)/sim_setup.mk
+include $(HW_DIR)/hardware.mk
+include $(SIM_DIR)/sim_setup.mk
 
 # import core software files
-include $(CORE_SW_DIR)/software.mk
+include $(SW_DIR)/software.mk
 
 # import document files
 ifneq ($(SETUP_DOC),0)
-include $(CORE_DOC_DIR)/doc_setup.mk
+include $(DOC_DIR)/doc_setup.mk
 endif
 
 setup: $(BUILD_DIR) $(SRC)
 ifneq ($(SETUP_SW),0)
-ifneq ($(wildcard $(CORE_PC_DIR)/*.expected),)
-	cp -u $(CORE_PC_DIR)/*.expected $(BUILD_SW_PC_DIR)
+ifneq ($(wildcard $(PC_DIR)/*.expected),)
+	cp -u $(PC_DIR)/*.expected $(BUILD_SW_PC_DIR)
 endif
-ifneq ($(wildcard $(CORE_PC_DIR)/pc-emul.mk),)
-	cp -u $(CORE_PC_DIR)/pc-emul.mk $(BUILD_SW_PC_DIR)
+ifneq ($(wildcard $(PC_DIR)/pc-emul.mk),)
+	cp -u $(PC_DIR)/pc-emul.mk $(BUILD_SW_PC_DIR)
 endif
-ifneq ($(wildcard $(CORE_EMB_DIR)/embedded.mk),)
-	cp -u $(CORE_EMB_DIR)/embedded.mk $(BUILD_SW_EMB_DIR)
+ifneq ($(wildcard $(EMB_DIR)/embedded.mk),)
+	cp -u $(EMB_DIR)/embedded.mk $(BUILD_SW_EMB_DIR)
 endif
 endif
 ifneq ($(SETUP_SIM),0)
-	cp -u $(CORE_SIM_DIR)/*.expected $(BUILD_SIM_DIR)
-ifneq ($(wildcard $(CORE_SIM_DIR)/simulation.mk),)
-	cp -u $(CORE_SIM_DIR)/simulation.mk $(BUILD_SIM_DIR)
+	cp -u $(SIM_DIR)/*.expected $(BUILD_SIM_DIR)
+ifneq ($(wildcard $(SIM_DIR)/simulation.mk),)
+	cp -u $(SIM_DIR)/simulation.mk $(BUILD_SIM_DIR)
 endif
-ifneq ($(wildcard $(CORE_SIM_DIR)/*_tb.*),)
-	cp -u $(CORE_SIM_DIR)/*_tb.* $(BUILD_SIM_DIR)
+ifneq ($(wildcard $(SIM_DIR)/*.cpp),)
+	cp -u $(SIM_DIR)/*.cpp $(BUILD_SIM_DIR)
+endif
+ifneq ($(wildcard $(SIM_DIR)/*.v),)
+	cp -u $(SIM_DIR)/*.v $(BUILD_SIM_DIR)
 endif
 endif
 ifneq ($(SETUP_FPGA),0)
-	cp -u $(CORE_FPGA_DIR)/*.expected $(BUILD_FPGA_DIR)
-ifneq ($(wildcard $(CORE_FPGA_DIR)/*.mk),)
-	cp -u $(CORE_FPGA_DIR)/*.mk $(BUILD_FPGA_DIR)
+	cp -u $(FPGA_DIR)/*.expected $(BUILD_FPGA_DIR)
+ifneq ($(wildcard $(FPGA_DIR)/*.mk),)
+	cp -u $(FPGA_DIR)/*.mk $(BUILD_FPGA_DIR)
 endif
-ifneq ($(wildcard $(CORE_FPGA_DIR)/*.sdc),)
-	cp -u $(CORE_FPGA_DIR)/*.sdc $(BUILD_FPGA_DIR)
+ifneq ($(wildcard $(FPGA_DIR)/*.sdc),)
+	cp -u $(FPGA_DIR)/*.sdc $(BUILD_FPGA_DIR)
 endif
-ifneq ($(wildcard $(CORE_FPGA_DIR)/*.xdc),)
-	cp -u $(CORE_FPGA_DIR)/*.xdc $(BUILD_FPGA_DIR)
+ifneq ($(wildcard $(FPGA_DIR)/*.xdc),)
+	cp -u $(FPGA_DIR)/*.xdc $(BUILD_FPGA_DIR)
 endif
 endif
 ifneq ($(SETUP_DOC),0)
-	cp -u $(CORE_DOC_DIR)/*.expected $(BUILD_DOC_DIR)
-ifneq ($(wildcard $(CORE_DOC_DIR)/*.mk),)
-	cp -u $(CORE_DOC_DIR)/*.mk $(BUILD_DOC_DIR)
+	cp -u $(DOC_DIR)/*.expected $(BUILD_DOC_DIR)
+ifneq ($(wildcard $(DOC_DIR)/*.mk),)
+	cp -u $(DOC_DIR)/*.mk $(BUILD_DOC_DIR)
 endif
-ifneq ($(wildcard $(CORE_DOC_DIR)/*.tex),)
-	cp -f $(CORE_DOC_DIR)/*.tex $(BUILD_TSRC_DIR)
+ifneq ($(wildcard $(DOC_DIR)/*.tex),)
+	cp -f $(DOC_DIR)/*.tex $(BUILD_TSRC_DIR)
 endif
-	cp -u $(CORE_DOC_DIR)/figures/* $(BUILD_FIG_DIR)
+	cp -u $(DOC_DIR)/figures/* $(BUILD_FIG_DIR)
 	cp -u $(LIB_DIR)/software/python/verilog2tex.py $(BUILD_SW_PYTHON_DIR)
 	cp -u $(LIB_DIR)/software/python/mkregs.py $(BUILD_SW_PYTHON_DIR)
 endif
