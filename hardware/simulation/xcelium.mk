6aa35043b897d3687dfe88b2d6a8c9a80b595c3e
Joao D Lopes <joao.d.lopes91@gmail.com>
2022-09-28 15:47:44 +0100

    remove INIT_SCRIPT for Xcelium

--- hardware/simulation/xcelium.mk
+++ hardware/simulation/xcelium.mk
@@ -2,19 +2,15 @@ SFLAGS =-errormax 15 -status
 VFLAGS+=$(SFLAGS) -update -linedebug -sv
 EFLAGS =$(SFLAGS) -access +wc
 
-ifneq ($(SIM_INIT_SCRIPT),)
-INIT_SCRIPT=set -e; source $(SIM_INIT_SCRIPT);
-endif
-
 ifeq ($(VCD),1)
 VFLAGS+=-define VCD
 endif
 
 comp: $(VHDR) $(VSRC)
-	$(INIT_SCRIPT) xmvlog $(VFLAGS) $(VSRC); xmelab $(EFLAGS) worklib.$(NAME)_tb:module
+	xmvlog $(VFLAGS) $(VSRC); xmelab $(EFLAGS) worklib.$(NAME)_tb:module
 
 exec:
-	$(INIT_SCRIPT) xmsim $(SFLAGS) worklib.$(NAME)_tb:module
+	xmsim $(SFLAGS) worklib.$(NAME)_tb:module
 	grep -v xcelium xmsim.log | grep -v xmsim | grep -v "\$finish" | tee -a test.log
 
 .PHONY: comp exec
