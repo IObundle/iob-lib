e870f618d19245e5c7c8637695626f7399c874a2
Joao D Lopes <joao.d.lopes91@gmail.com>
2022-10-19 16:29:34 +0100

    add inversion port in iob_clkbuf module

--- hardware/iob_clkbuf/iob_clkbuf.v
+++ hardware/iob_clkbuf/iob_clkbuf.v
@@ -3,17 +3,22 @@
 module iob_clkbuf
   (
    input  clk_i,
+   input  n_i,
    output clk_o
    );
+
+   wire   clk_int;
 
 `ifdef XILINX
    BUFG BUFG_inst
      (
       .I(clk_i),
-      .O(clk_o)
+      .O(clk_int)
       );
 `else
-   assign clk_o = clk_i;
+   assign clk_int = clk_i;
 `endif
+
+   assign clk_o = n_i^clk_int;
 
 endmodule
