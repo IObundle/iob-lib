d9dfea65f849eacb4548fd84dd408aeb1bc08ebe
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-11-02 18:23:46 +0000

    fix xcelium issues

--- hardware/iob_pulse_gen/iob_pulse_gen.v
+++ hardware/iob_pulse_gen/iob_pulse_gen.v
@@ -17,13 +17,15 @@ module iob_pulse_gen
    `IOB_WIRE(restart_detect, 1)
    iob_pulse_detect pulse_detect (clk_i, arst_i, restart_i, restart_detect);
 
-   //counter enable
+   //counter
    `IOB_WIRE(cnt_en, 1)
+   localparam WIDTH = $clog2(START+DURATION+2);
+   `IOB_WIRE(cnt, WIDTH)   
+
+   //counter enable
    assign cnt_en = restart_detect & (cnt <= (START+DURATION));
 
    //counter
-   localparam WIDTH = $clog2(START+DURATION+2);
-   `IOB_WIRE(cnt, WIDTH)   
    iob_counter #(WIDTH,0) cnt0 
      (clk_i, arst_i, restart_i, cnt_en, 1'b0, {WIDTH{1'b0}}, cnt);
 
