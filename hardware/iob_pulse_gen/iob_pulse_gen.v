6901b76b22b62694d5ef4e3ce11d6081bc896aa4
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-11-21 19:31:53 +0000

    fix bugs

--- hardware/iob_pulse_gen/iob_pulse_gen.v
+++ hardware/iob_pulse_gen/iob_pulse_gen.v
@@ -9,25 +9,27 @@ module iob_pulse_gen
   (
    input  clk_i,
    input  arst_i,
-   input  restart_i,
+   input  rst_i,
+   input  start_i,
    output pulse_o
    );
 
-   //restart detect
-   `IOB_WIRE(restart_detect, 1)
-   iob_pulse_detect pulse_detect (clk_i, arst_i, restart_i, restart_detect);
+   //start detect
+   `IOB_WIRE(start_detected, 1)
+   iob_reg #(1,0) start_detected_inst (clk_i, arst_i, 1'b0, start_i, 1'b1, start_detected);
 
+   
    //counter
    `IOB_WIRE(cnt_en, 1)
    localparam WIDTH = $clog2(START+DURATION+2);
    `IOB_WIRE(cnt, WIDTH)   
 
    //counter enable
-   assign cnt_en = restart_detect & (cnt <= (START+DURATION));
+   assign cnt_en = start_detected & (cnt <= (START+DURATION));
 
    //counter
    iob_counter #(WIDTH,0) cnt0 
-     (clk_i, arst_i, restart_i, cnt_en, 1'b0, {WIDTH{1'b0}}, cnt);
+     (clk_i, arst_i, start_i, cnt_en, 1'b0, {WIDTH{1'b0}}, cnt);
 
    //pulse
    assign pulse_o = cnt_en & |cnt;
