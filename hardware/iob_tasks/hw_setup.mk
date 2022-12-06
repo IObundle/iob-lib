66c04b2f88af6b2ab36c0326a696e9f4ed465f38
Joao D Lopes <joao.d.lopes91@gmail.com>
2022-12-06 11:46:10 +0000

    fix simulation build directory path in hw_setup.mk files

--- hardware/iob_tasks/hw_setup.mk
+++ hardware/iob_tasks/hw_setup.mk
@@ -4,8 +4,8 @@ ifeq ($(filter iob_tasks, $(HW_MODULES)),)
 HW_MODULES+=iob_tasks
 
 # Sources
-SRC+=$(BUILD_SIM_DIR)/iob_tasks.vh
-$(BUILD_SIM_DIR)/iob_tasks.vh: $(LIB_DIR)/hardware/iob_tasks/iob_tasks.vh
+SRC+=$(BUILD_SIM_DIR)/src/iob_tasks.vh
+$(BUILD_SIM_DIR)/src/iob_tasks.vh: $(LIB_DIR)/hardware/iob_tasks/iob_tasks.vh
 	cp $< $@
 
 endif
