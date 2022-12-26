353a95b18ea1dea44db7fbf6906b95678e70ba93
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-12-26 19:30:05 +0000

    assume genus env set: fix

--- hardware/syn/tsmc_cln28hpc/tsmc_cln28hpc.mk
+++ hardware/syn/tsmc_cln28hpc/tsmc_cln28hpc.mk
@@ -7,7 +7,7 @@ SYN_SCP_FLAGS=$(CADENCE_SCP_FLAGS)
 SYN_SYNC_FLAGS=$(CADENCE_SYNC_FLAGS)
 
 synth: $(VHDR) $(VSRC)
-	$(GENUS_ENV); genus -batch -files run_genus.tcl
+	genus -batch -files run_genus.tcl
 
 tsmc_cln28hpc-clean:
 	@rm -rf fv results st_part_log
