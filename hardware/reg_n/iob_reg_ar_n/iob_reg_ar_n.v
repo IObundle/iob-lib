fe7e97cbf944c40b57332710febc0e9b55d79582
Joao D Lopes <joao.d.lopes91@gmail.com>
2022-12-30 16:09:11 +0000

    fix modules name: use _n as a suffix

--- hardware/reg_n/iob_reg_ar_n/iob_reg_ar_n.v
+++ hardware/reg_n/iob_reg_ar_n/iob_reg_ar_n.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module iob_reg_n_a
+module iob_reg_ar_n
   #(
     parameter DATA_W = 0,
     parameter RST_VAL = 0
@@ -8,6 +8,7 @@ module iob_reg_n_a
    (
     input                   clk_i,
     input                   arst_i,
+    input                   rst_i,
     input [DATA_W-1:0]      data_i,
     output reg [DATA_W-1:0] data_o
     );
@@ -17,6 +18,8 @@ module iob_reg_n_a
    
    always @(negedge clk_i, posedge arst_i) begin
       if (arst_i) begin
+         data_o <= RST_VAL_INT;
+      end else if (rst_i) begin
          data_o <= RST_VAL_INT;
       end else begin
          data_o <= data_i;
