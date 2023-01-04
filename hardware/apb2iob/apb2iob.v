0096fb1d393a109aa9186dcd238ac7d98250778d
AndreMerendeira <andre.merendeira@gmail.com>
2023-01-04 18:42:35 +0000

    [APB2IOB] fixed FSM

--- hardware/apb2iob/apb2iob.v
+++ hardware/apb2iob/apb2iob.v
@@ -24,12 +24,7 @@ module apb2iob
 
 
    // APB outputs
-
-   `IOB_WIRE(apb_ready_nxt, 1)
-   `IOB_VAR(apb_slverr_nxt, 1)
-   `IOB_VAR(iob_avalid_nxt, 1)
-
-   assign apb_ready_nxt = apb_write_i? (iob_avalid_nxt & iob_ready_nxt_i): iob_rvalid_nxt_i;
+   `IOB_VAR(apb_ready_nxt, 1)
 
    iob_reg
      #(1,0)
@@ -44,33 +39,10 @@ module apb2iob
 
    assign apb_rdata_o = iob_rdata_i;
 
+   `IOB_WIRE(pc, 1)
+   `IOB_VAR(pc_nxt, 1)
    iob_reg
      #(1,0)
-   apb_slverr_reg_inst
-     (
-      .clk_i(clk_i),
-      .arst_i(arst_i),
-      .cke_i(cke_i),
-      .data_i(apb_slverr_nxt),
-      .data_o(apb_slverr_o)
-      );
-
-   // IOb outputs
-   iob_reg
-     #(1,0)
-   avlid_reg
-     (
-      .clk_i(clk_i),
-      .arst_i(arst_i),
-      .cke_i(cke_i),
-      .data_i(iob_avalid_nxt),
-      .data_o(iob_avalid_o)
-      );
-
-   `IOB_WIRE(pc, 2)
-   `IOB_VAR(pc_nxt, 2)
-   iob_reg
-     #(2,0)
    pc_reg
      (
       .clk_i(clk_i),
@@ -79,41 +51,32 @@ module apb2iob
       .data_i(pc_nxt),
       .data_o(pc)
       );
+      
+   `IOB_VAR(iob_avalid_int, 1)
 
    `IOB_COMB begin
       
       pc_nxt = pc+1'b1;
-      iob_avalid_nxt = iob_avalid_o;
-      apb_slverr_nxt = 1'b0;
-      
+      iob_avalid_int = 1'd1;
+      apb_ready_nxt = apb_write_i? iob_ready_nxt_i : iob_rvalid_nxt_i;
+   
       case(pc)
         0: begin
-           if(!apb_sel_i) //wait periph selection
+           if(!apb_sel_i) begin//wait periph selection
              pc_nxt = pc;
-           else
-             iob_avalid_nxt = 1'b1;
+             iob_avalid_int = 1'b0;
+             apb_ready_nxt = 1'd0;
+           end
         end
 
-        1: begin
-           if (!apb_enable_i)
-             apb_slverr_nxt = 1'b1;
-           if(!iob_ready_nxt_i) //wait until iob interface is ready
+        default: begin
+           if (apb_enable_i && !apb_ready_o)
              pc_nxt = pc;
-           else
-             iob_avalid_nxt = 1'b0; //deassert valid - transaction will be done
-        end
-        
-        default: begin //wait apb transaction to finish
-           if(apb_sel_i)
-             pc_nxt = pc;
-           else begin
-              apb_slverr_nxt = 1'b0;
-              pc_nxt = 0;
-           end
         end
       endcase
    end
 
+   assign iob_avalid_o = iob_avalid_int;
    assign iob_addr_o  = apb_addr_i;
    assign iob_wdata_o = apb_wdata_i;
    assign iob_wstrb_o = apb_wstrb_i;
