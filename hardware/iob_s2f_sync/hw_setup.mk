1adc1745be3415fa206b4bbf52d9f225ca693292
Joao D Lopes <joao.d.lopes91@gmail.com>
2023-01-04 15:41:05 +0000

    rename all en_i to cke_i; remove all always posedge clock

--- hardware/iob_s2f_sync/hw_setup.mk
+++ hardware/iob_s2f_sync/hw_setup.mk
@@ -3,6 +3,9 @@ ifeq ($(filter iob_s2f_sync, $(HW_MODULES)),)
 # Add to modules list
 HW_MODULES+=iob_s2f_sync
 
+# Submodules
+include $(LIB_DIR)/hardware/reg/iob_reg_r/hw_setup.mk
+
 # Sources
 SRC+=$(BUILD_VSRC_DIR)/iob_s2f_sync.v
 
