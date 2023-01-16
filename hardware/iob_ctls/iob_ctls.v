894718a621cc01ba5a26e29e0c8ddd4cf9d0bd69
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2023-01-16 01:47:12 +0000

    add suport for vcs and questa simulators; improve coverage and linting
    remove modules used for simulation from fifo, and create sim_setup.mk
    small fixes to xcelium simulation file

--- hardware/iob_ctls/iob_ctls.v
+++ hardware/iob_ctls/iob_ctls.v
@@ -40,21 +40,19 @@ module iob_ctls
      assign data_int2 = data_int1;   
    endgenerate
 
-   //normalized to count trailing ones
+   //normalized to count trailing zeros
    reg found_one;
    reg [$clog2(N):0] count;
-   reg [$clog2(N):0] count_val;
    integer           i;
    
    always @* begin
       found_one = 0;
       count = 0;
+      
       for (i=0; i < N; i=i+1) begin
-         count_val = i+1;
+         found_one = found_one | data_int2[i];
          if (!found_one)
-           found_one = data_int2[i];
-         if (!found_one)
-           count=count_val;
+            count = i+1;
       end
    end
    
