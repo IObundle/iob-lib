a0fe968fc96c3acbcb6fa3a842d6b7423be5f343
Pedro Antunes <pedronmantunes@gmail.com>
2023-01-22 23:44:54 +0000

    Run in FPGA: iob-soc builds for CYCLONE but run blocks;

--- hardware/fpga/quartus/quartus.mk
+++ hardware/fpga/quartus/quartus.mk
@@ -5,15 +5,17 @@ FPGA_OBJ:=$(NAME).qxp
 endif
 
 FPGA_LOG=quartus.log
-
+NORUN=0
 
 FPGA_SERVER=$(QUARTUS_SERVER)
 FPGA_USER=$(QUARTUS_USER)
 FPGA_ENV=$(QUARTUSPATH)/nios2eds/nios2_command_shell.sh
+FPGA_PROG=$(FPGA_ENV) quartus_pgm -m jtag -c 1 -o 'p;iob_soc.sof'
 
 $(FPGA_OBJ): $(VHDR) $(VSRC) $(wildcard *.sdc)
 	$(FPGA_ENV) quartus_sh -t quartus/quartus.tcl $(NAME) $(BOARD) "$(VSRC)" $(IS_FPGA)
 	@mv output_files/*.fit.summary $(FPGA_LOG)
+	@mv output_files/$(FPGA_OBJ) $(FPGA_OBJ)
 
 quartus-clean:
 	@rm -rf incremental_db db output_files
--- hardware/ram/iob_ram_dp_be/iob_ram_dp_be.v
+++ hardware/ram/iob_ram_dp_be/iob_ram_dp_be.v
@@ -7,7 +7,8 @@ module iob_ram_dp_be
   #(
     parameter HEXFILE = "none",
     parameter ADDR_W = 10, // Addr Width in bits : 2*ADDR_W = RAM Depth
-    parameter DATA_W = 32  // Data Width in bits
+    parameter DATA_W = 32,  // Data Width in bits
+    parameter IOB_MEM_NO_READ_ON_WRITE = 1
     )
    (
     input                clk_i,
@@ -30,80 +31,80 @@ module iob_ram_dp_be
    localparam COL_W = 8;
    localparam NUM_COL = DATA_W/COL_W;
 
-`ifdef IOB_MEM_NO_READ_ON_WRITE
-   localparam file_suffix = {"7","6","5","4","3","2","1","0"};
-
-   genvar                    i;
+   genvar index;
    generate
-      for (i=0; i < NUM_COL; i=i+1) begin: ram_col
-         localparam mem_init_file_int = (HEXFILE != "none")? {HEXFILE, "_", file_suffix[8*(i+1)-1 -: 8], ".hex"}: "none";
+   if(IOB_MEM_NO_READ_ON_WRITE) begin: with_IOB_MEM_NO_READ_ON_WRITE
+    localparam file_suffix = {"7","6","5","4","3","2","1","0"};
+    for (index=0; index < NUM_COL; index=index+1) begin: ram_col
+        localparam mem_init_file_int = (HEXFILE != "none")? {HEXFILE, "_", file_suffix[8*(index+1)-1 -: 8], ".hex"}: "none";
 
-         iob_ram_dp
-             #(
-               .HEXFILE(mem_init_file_int),
-               .ADDR_W(ADDR_W),
-               .DATA_W(COL_W)
-               ) ram
-           (
+        iob_ram_dp
+            #(
+            .HEXFILE(mem_init_file_int),
+            .ADDR_W(ADDR_W),
+            .DATA_W(COL_W),
+            .IOB_MEM_NO_READ_ON_WRITE(IOB_MEM_NO_READ_ON_WRITE)
+            ) ram
+        (
             .clk_i   (clk_i),
 
             .enA_i   (enA_i),
             .addrA_i (addrA_i),
-            .dA_i    (dA_i[i*COL_W +: COL_W]),
-            .weA_i   (weA_i[i]),
-            .dA_o    (dA_o[i*COL_W +: COL_W]),
+            .dA_i    (dA_i[index*COL_W +: COL_W]),
+            .weA_i   (weA_i[index]),
+            .dA_o    (dA_o[index*COL_W +: COL_W]),
 
             .enB_i   (enB_i),
             .addrB_i (addrB_i),
-            .dB_i    (dB_i[i*COL_W +: COL_W]),
-            .weB_i   (weB_i[i]),
-            .dB_o    (dB_o[i*COL_W +: COL_W])
+            .dB_i    (dB_i[index*COL_W +: COL_W]),
+            .weB_i   (weB_i[index]),
+            .dB_o    (dB_o[index*COL_W +: COL_W])
             );
-      end
+    end
+   end else begin: not_IOB_MEM_NO_READ_ON_WRITE
+    // this allow ISE 14.7 to work; do not remove
+    localparam mem_init_file_int = {HEXFILE, ".hex"};
+
+    // Core Memory
+    reg [DATA_W-1:0]      ram_block[(2**ADDR_W)-1:0];
+
+    // Initialize the RAM
+    initial
+        if(mem_init_file_int != "none.hex")
+        $readmemh(mem_init_file_int, ram_block, 0, 2**ADDR_W - 1);
+
+    // Port-A Operation
+    reg [DATA_W-1:0]      dA_o_int;
+    integer                   i;
+    always @(posedge clk_i) begin
+        if (enA_i) begin
+            for (i=0; i < NUM_COL; i=i+1) begin
+                if (weA_i[i]) begin
+                ram_block[addrA_i][i*COL_W +: COL_W] <= dA_i[i*COL_W +: COL_W];
+                end
+            end
+            dA_o_int <= ram_block[addrA_i]; // Send Feedback
+        end
+    end
+
+    assign dA_o = dA_o_int;
+
+    // Port-B Operation
+    reg [DATA_W-1:0]      dB_o_int;
+    integer                   j;
+    always @(posedge clk_i) begin
+        if (enB_i) begin
+            for (j=0; j < NUM_COL; j=j+1) begin
+                if (weB_i[j]) begin
+                ram_block[addrB_i][j*COL_W +: COL_W] <= dB_i[j*COL_W +: COL_W];
+                end
+            end
+            dB_o_int <= ram_block[addrB_i]; // Send Feedback
+        end
+    end
+
+    assign dB_o = dB_o_int;
+   end
    endgenerate
-`else // !IOB_MEM_NO_READ_ON_WRITE
-   // this allow ISE 14.7 to work; do not remove
-   localparam mem_init_file_int = {HEXFILE, ".hex"};
-
-   // Core Memory
-   reg [DATA_W-1:0]      ram_block[(2**ADDR_W)-1:0];
-
-   // Initialize the RAM
-   initial
-     if(mem_init_file_int != "none.hex")
-       $readmemh(mem_init_file_int, ram_block, 0, 2**ADDR_W - 1);
-
-   // Port-A Operation
-   reg [DATA_W-1:0]      dA_o_int;
-   integer                   i;
-   always @(posedge clk_i) begin
-      if (enA_i) begin
-         for (i=0; i < NUM_COL; i=i+1) begin
-            if (weA_i[i]) begin
-               ram_block[addrA_i][i*COL_W +: COL_W] <= dA_i[i*COL_W +: COL_W];
-            end
-         end
-         dA_o_int <= ram_block[addrA_i]; // Send Feedback
-      end
-   end
-
-   assign dA_o = dA_o_int;
-
-   // Port-B Operation
-   reg [DATA_W-1:0]      dB_o_int;
-   integer                   j;
-   always @(posedge clk_i) begin
-      if (enB_i) begin
-         for (j=0; j < NUM_COL; j=j+1) begin
-            if (weB_i[j]) begin
-               ram_block[addrB_i][j*COL_W +: COL_W] <= dB_i[j*COL_W +: COL_W];
-            end
-         end
-         dB_o_int <= ram_block[addrB_i]; // Send Feedback
-      end
-   end
-
-   assign dB_o = dB_o_int;
-`endif
 
 endmodule
