d1bd6c44d330efaa43ab5144fb3581adbe927de9
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2023-02-06 02:00:36 +0000

    compile CYCLONEV-GT FPGA with DDR3; merge device and io tcl files for same board

--- hardware/fpga/quartus/CYCLONEV-GT-DK/board.mk
+++ hardware/fpga/quartus/CYCLONEV-GT-DK/board.mk
@@ -1,11 +1,10 @@
 BOARD_SERVER=$(CYC5_SERVER)
 BOARD_USER=$(CYC5_USER)
 
-QIP_FILE=$(FPGA_TOOL)/$(BOARD)/alt_ddr3.qip
-
 ifeq ($(USE_EXTMEM),1)
-FPGA_DEPS+=$(QIP_FILE)
-endif
+QIP_FILE=$(FPGA_TOOL)/$(BOARD)/alt_ddr3/synthesis/alt_ddr3.qip
+QIP+=$(QIP_FILE)
 
 $(QIP_FILE): $(FPGA_TOOL)/$(BOARD)/alt_ddr3.qsys
 	$(FPGA_ENV) qsys-generate --synthesis $<
+endif
