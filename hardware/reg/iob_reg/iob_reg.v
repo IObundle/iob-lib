e1e65865ff54b13212279b18541bf40ae8f46e3e
AndreMerendeira <andre.merendeira@gmail.com>
2023-02-14 17:15:31 +0000

    [Lint] Linted all the iob_regs

--- hardware/reg/iob_reg/iob_reg.v
+++ hardware/reg/iob_reg/iob_reg.v
@@ -1,25 +1,25 @@
 `timescale 1ns / 1ps
 
 module iob_reg
-   #(
-   parameter DATA_W = 21,
-   parameter RST_VAL = {DATA_W{1'b0}}
-   )
-   (
-   input                   clk_i,
-   input                   arst_i,
-   input                   cke_i,
+  #(
+    parameter DATA_W = 21,
+    parameter RST_VAL = {DATA_W{1'b0}}
+  )
+  (
+    input                   clk_i,
+    input                   arst_i,
+    input                   cke_i,
 
-   input [DATA_W-1:0]      data_i,
-   output reg [DATA_W-1:0] data_o
-   );
-   
-   always @(posedge clk_i, posedge arst_i) begin
-      if (arst_i) begin
-         data_o <= RST_VAL;
-      end else if (cke_i) begin
-         data_o <= data_i;
-      end
-   end
+    input [DATA_W-1:0]      data_i,
+    output reg [DATA_W-1:0] data_o
+  );
+  
+  always @(posedge clk_i, posedge arst_i) begin
+    if (arst_i) begin
+      data_o <= RST_VAL;
+    end else if (cke_i) begin
+      data_o <= data_i;
+    end
+  end
 
 endmodule
--- hardware/reg/iob_reg_re/iob_reg_re.v
+++ hardware/reg/iob_reg_re/iob_reg_re.v
@@ -2,10 +2,10 @@
 
 module iob_reg_re
   #(
-    parameter DATA_W = 0,
-    parameter RST_VAL = 0
-    )
-   (
+    parameter DATA_W = 21,
+    parameter RST_VAL = {DATA_W{1'b0}}
+  )
+  (
     input               clk_i,
     input               arst_i,
     input               cke_i,
@@ -15,21 +15,21 @@ module iob_reg_re
 
     input [DATA_W-1:0]  data_i,
     output [DATA_W-1:0] data_o
-    );
+  );
 
-   wire [DATA_W-1:0]    data;
-   assign data = en_i? data_i: data_o;
+  wire [DATA_W-1:0]    data;
+  assign data = en_i? data_i: data_o;
 
-   iob_reg_r #(DATA_W, RST_VAL) reg0
-     (
-      .clk_i(clk_i),
-      .arst_i(arst_i),
-      .cke_i(cke_i),
+  iob_reg_r #(DATA_W, RST_VAL) reg0
+    (
+    .clk_i(clk_i),
+    .arst_i(arst_i),
+    .cke_i(cke_i),
 
-      .rst_i(rst_i),
+    .rst_i(rst_i),
 
-      .data_i(data),
-      .data_o(data_o)
-      );
+    .data_i(data),
+    .data_o(data_o)
+  );
 
 endmodule
