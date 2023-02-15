34bfc8a7c0500bde889379132694b96851c22077
AndreMerendeira <andre.merendeira@gmail.com>
2023-02-15 16:45:41 +0000

    [Lint] Linted all counters

--- hardware/counter/iob_counter_ld/iob_counter_ld.v
+++ hardware/counter/iob_counter_ld/iob_counter_ld.v
@@ -3,10 +3,10 @@
 
 module iob_counter_ld
   #(
-    parameter DATA_W = 32,
-    parameter RST_VAL = 0
-    )
-   (
+    parameter DATA_W = 21,
+    parameter RST_VAL = {DATA_W{1'b0}}
+  )
+  (
     input               clk_i,
     input               arst_i,
     input               cke_i,
@@ -18,22 +18,22 @@ module iob_counter_ld
     input [DATA_W-1:0]  ld_val_i,
 
     output [DATA_W-1:0] data_o
-    );
+  );
 
-   wire [DATA_W-1:0]    data;
-   assign data = ld_i? ld_val_i: data_o + 1'b1;
+  wire [DATA_W-1:0]    data;
+  assign data = ld_i? ld_val_i: data_o + 1'b1;
 
-   iob_reg_re #(DATA_W, RST_VAL) reg0
-     (
-      .clk_i(clk_i),
-      .arst_i(arst_i),
-      .cke_i(cke_i),
+  iob_reg_re #(DATA_W, RST_VAL) reg0
+  (
+    .clk_i(clk_i),
+    .arst_i(arst_i),
+    .cke_i(cke_i),
 
-      .rst_i(rst_i),
-      .en_i(en_i),
+    .rst_i(rst_i),
+    .en_i(en_i),
 
-      .data_i(data),
-      .data_o(data_o)
-      );
+    .data_i(data),
+    .data_o(data_o)
+  );
 
 endmodule
