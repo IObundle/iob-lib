d081e63c966125d8b511aad91e2200549d3643d4
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2023-03-08 01:58:42 +0000

    add cdc bus skew and metastability

--- hardware/fpga/quartus/timing.tcl
+++ hardware/fpga/quartus/timing.tcl
@@ -4,7 +4,9 @@ project_open -force $NAME -revision $NAME
 create_timing_netlist -model slow
 read_sdc
 update_timing_netlist
-report_path -nworst 50 -multi_corner -file reports/timing_paths.txt
-report_path -min_path -file reports/min_path.txt
+report_path -nworst 50 -multi_corner -file reports/$NAME.sta.paths
+report_path -min_path -file reports/$NAME.sta.min_path
+report_max_skew -file reports/$NAME.sta.skew
+report_metastability -file reports/$NAME.sta.metastability
 catch {delete_timing_netlist}
 
