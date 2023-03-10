524407941a2fd8b02b37a423be60f7f3dd510347
zettasticks <ruben.teixeira@tecnico.ulisboa.pt>
2023-03-10 02:26:32 +0000

    Final finishes on Makefile

--- hardware/modules/axis2axi/axis2axi.py
+++ hardware/modules/axis2axi/axis2axi.py
@@ -1,2 +1,2 @@
 v_headers     = [ 'm_axi_m_port', 'm_axi_write_m_port', 'm_axi_read_m_port', 'm_axi_write_portmap', 'm_axi_read_portmap' ]
-hw_modules    = [ 'axis2axi.v', 'axis2axi_in.v', 'axis2axi_out.v', 'iob_fifo_sync' ]
+hw_modules    = [ 'axis2axi.v', 'axis2axi_in.v', 'axis2axi_out.v', 'iob_fifo_sync' , 'iob_counter' , 'iob_reg_r' , 'iob_reg_re' , 'iob_asym_converter']
