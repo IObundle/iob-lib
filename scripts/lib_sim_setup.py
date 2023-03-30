bf0bd79d5277b55f25beb2bb508b172c78766fab
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-03-30 23:07:05 +0100

    feat(test): Fix lib_sim_setup.py

--- scripts/lib_sim_setup.py
+++ scripts/lib_sim_setup.py
@@ -1,4 +1,4 @@
-from build_srcs import lib_module_setup, create_Vheaders, copy_files, module_dependency_setup
+from build_srcs import lib_module_setup, create_if_gen_headers, copy_files, module_dependency_setup
 import sys
 
 def lib_sim_setup (module_name, dest_srcs_dir):
@@ -7,10 +7,12 @@ def lib_sim_setup (module_name, dest_srcs_dir):
     sim_srcs = []
     lib_dir="."
     lib_module_setup(Vheaders, hardware_srcs, module_name, lib_dir=lib_dir, add_sim_srcs=True)
-    module_dependency_setup(hardware_srcs, Vheaders, "src", [], lib_dir=lib_dir, add_sim_srcs=True)
+    module_dependency_setup(hardware_srcs, Vheaders, "src", [], lib_dir=lib_dir, function_2_call='sim_setup') #function_2_call is purely used to add_sim_srcs (the function does not actually get called)
 
     #Copy Hw
-    if Vheaders: create_Vheaders( dest_srcs_dir, Vheaders )
+    if Vheaders: 
+        create_if_gen_headers( dest_srcs_dir, Vheaders )
+        copy_files( LIB_DIR, dest_srcs_dir, Vheaders, '*.vh' )
     if hardware_srcs: copy_files( lib_dir, dest_srcs_dir, hardware_srcs, '*.v' )
     
     # Copy TB
