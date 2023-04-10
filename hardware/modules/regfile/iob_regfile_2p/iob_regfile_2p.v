2fe85fe7668dd5e7a9988fcab5ac9b280f113766
AndreMerendeira <andre.merendeira@gmail.com>
2023-04-10 15:44:19 +0100

    [regfile_2p] fixed mem

--- hardware/modules/regfile/iob_regfile_2p/iob_regfile_2p.v
+++ hardware/modules/regfile/iob_regfile_2p/iob_regfile_2p.v
@@ -17,26 +17,23 @@ module iob_regfile_2p
     input [DATA_W-1:0]        wdata_i,
 
     // Read Port
-    input                     ren_i,
     input [ADDR_W_INT-1:0]    raddr_i,
     output [DATA_W-1:0]       rdata_o
   );
 
-  wire [((2**ADDR_W)*DATA_W)-1 :0] regfile;
+  reg [DATA_W-1:0] regfile [(2**ADDR_W)-1:0];
   
   genvar addr;
   generate
-    for (addr=0; addr < (2**ADDR_W); addr=addr+1) begin: rf
+    for (addr=0; addr < (2**ADDR_W); addr=addr+1) begin: register_file
       always @(posedge clk_i)
         if (rst_i)
           regfile[addr] <= {DATA_W{1'b0}};
-        else if (wen_i && (addr == addr_i))
-          regfile[addr] <= wdata_i;
+        else if (wen_i && (waddr_i == addr))
+          regfile[addr] <= w_data_i;
     end
   endgenerate
 
-  wire [ADDR_W_INT-1:0] raddr = (ren_i == 1'd0) ? raddr_i : {ADDR_W_INT{1'b0}};
-  
-  assign rdata_o = regfile[(raddr*DATA_W)+:DATA_W];
+  assign rdata_o = regfile[raddr_i];
   
 endmodule
