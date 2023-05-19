f41b2d8397e9217e279ca64627361d9a892dc3a5
Pedro Antunes <pedronmantunes@gmail.com>
2023-05-19 17:11:19 +0200

    verilog-format.sh: fix not to through an error when no files are passed

--- scripts/verilog-format.sh
+++ scripts/verilog-format.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 # run the command below for all files given as command line arguments
-verible-verilog-format --inplace `cat $IOB_LIB_PATH/verible-format.rules | tr '\n' ' '` $@
+if [ "$#" -eq 0 ]; then
+    echo "No files specified."
+    exit 0
+fi
+
+verible-verilog-format --inplace $(cat "$IOB_LIB_PATH/verible-format.rules" | tr '\n' ' ') "$@"
