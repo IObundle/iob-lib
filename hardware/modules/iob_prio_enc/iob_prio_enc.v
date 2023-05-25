914e2c1fc718d8d9563cc4621671f8787da87db7
AndreMerendeira <andre.merendeira@gmail.com>
2023-05-25 16:43:18 +0100

    [iob_prio] added missing generate label

--- hardware/modules/iob_prio_enc/iob_prio_enc.v
+++ hardware/modules/iob_prio_enc/iob_prio_enc.v
@@ -12,7 +12,7 @@ module iob_prio_enc #(
 
    integer pos;
    generate
-      if (PRIO == "LOWEST") begin
+      if (PRIO == "LOWEST") begin : gen_lowest_prio
          always @* begin
             encoded_o = {($clog2(WIDTH) + 1) {1'd0}};  //In case input is 0
             for (pos = WIDTH - 1; pos != -1; pos = pos - 1) begin
@@ -21,7 +21,7 @@ module iob_prio_enc #(
                end
             end
          end
-      end else begin  //PRIO == "HIGHEST"
+      end else begin : gen_highest_prio  //PRIO == "HIGHEST"
          always @* begin
             encoded_o = WIDTH;  //In case input is 0
             for (pos = 0; pos != WIDTH; pos = pos + 1) begin
