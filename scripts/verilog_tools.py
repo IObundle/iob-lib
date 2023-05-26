5c2d388180af6fe0af9cfca2212a38a0109c0aea
Pedro Antunes <pedronmantunes@gmail.com>
2023-05-26 11:34:27 +0200

    verilog_tools.py: fix not replacing in .vs files;

--- scripts/verilog_tools.py
+++ scripts/verilog_tools.py
@@ -21,11 +21,12 @@ def replace_includes(setup_dir="", build_dir=""):
         for file in files:
             if file.endswith(".vs"):
                 VSnippetFiles.append(f"{root}/{file}")
-            elif file.endswith(".v") or file.endswith(".sv") or file.endswith(".vh"):
+            elif file.endswith(".v") or file.endswith(".sv") or file.endswith(".vh") or file.endswith(".vs"):
                 VerilogFiles.append(f"{root}/{file}")
 
     for VSnippetFile in VSnippetFiles:
         head, tail = os.path.split(VSnippetFile)
+        VerilogFiles.remove(VSnippetFile)
         with open(VSnippetFile, "r") as snippet:
             code = snippet.read()
         for VerilogFile in VerilogFiles:
