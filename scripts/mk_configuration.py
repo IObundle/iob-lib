b750d4f9c53b7f0302ca699a55ba3080575e1652
AndreMerendeira <andre.merendeira@gmail.com>
2023-06-01 15:09:49 +0100

    [mk_configuration] removed macro from params inst

--- scripts/mk_configuration.py
+++ scripts/mk_configuration.py
@@ -20,11 +20,10 @@ def params_vh(params, top_module, out_dir):
     file2create.close()
 
     file2create = open(f"{out_dir}/{top_module}_inst_params.vs", "w")
-    core_prefix = f"{top_module}_".upper()
     for parameter in params:
         if parameter["type"] in ["P", "F"]:
             p_name = parameter["name"].upper()
-            file2create.write(f"\n  .{p_name}(`{core_prefix}{p_name}),")
+            file2create.write(f"\n  .{p_name}({p_name}),")
     file2create = open(f"{out_dir}/{top_module}_inst_params.vs", "rb+")
     file2create.seek(-1, os.SEEK_END)
     file2create.write(b"\n")
