021e3532a006f9b93ba5cc6904788149a58c7f71
AndreMerendeira <andre.merendeira@gmail.com>
2023-06-05 09:30:56 +0100

    [demux] removed unused comment

--- hardware/modules/iob_demux/iob_demux.v
+++ hardware/modules/iob_demux/iob_demux.v
@@ -2,25 +2,15 @@
 
 
 module iob_demux #(
-    parameter DATA_W = 21,
-    parameter N      = 21
+   parameter DATA_W = 21,
+   parameter N      = 21
 ) (
-    input  [($clog2(N)+($clog2(N)==0))-1:0] sel_i,
-    input  [                    DATA_W-1:0] data_i,
-    output [                (N*DATA_W)-1:0] data_o
+   input  [($clog2(N)+($clog2(N)==0))-1:0] sel_i,
+   input  [                    DATA_W-1:0] data_i,
+   output [                (N*DATA_W)-1:0] data_o
 );
 
-   //integer i;
-   //always @* begin
-   //    for (i=0; i<N; i=i+1)
-   //        if(i == sel_i) begin
-   //            data_o[i*DATA_W += DATA_W] = data_i;
-   //        end else begin
-   //            data_o[i*DATA_W += DATA_W] = {(DATA_W){1'b0}};
-   //        end
-   //end
-
-   //Alternative
+   //Select the data to output
    assign data_o = {{((N - 1) * DATA_W) {1'b0}}, data_i} << (sel_i * DATA_W);
 
 endmodule
