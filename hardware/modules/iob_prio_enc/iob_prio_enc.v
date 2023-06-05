5f1df4ca2b8eb9f80ce53ba39d3f1620e50bcd7f
AndreMerendeira <andre.merendeira@gmail.com>
2023-06-05 11:01:42 +0100

    [prio_encoder] removed unecessary encoder and added MODE param

--- hardware/modules/iob_prio_enc/iob_prio_enc.v
+++ hardware/modules/iob_prio_enc/iob_prio_enc.v
@@ -1,36 +1,35 @@
 `timescale 1ns / 1ps
 
-
 module iob_prio_enc #(
-    parameter W = 21
+   parameter W    = 21,
+   // Priority: "LOW", "HIGH"
+   parameter MODE = "LOW"  //"LOW" -> smaller index
 ) (
-   input [ W-1:0]         unencoded_i,
-   output [$clog2(W)-1:0] encoded_o
+   input      [            WIDTH-1:0] unencoded_i,
+   output reg [($clog2(WIDTH)+1)-1:0] encoded_o
 );
 
-   wire [W-1:0]                        filter1;
-   wire [W-1:0]                        filter2;
-
-   assign filter1[0] = unencoded_i[0];
-   assign filter2[0] = unencoded_i[0];
-   
-   genvar                              i;
-   
+   integer pos;
    generate
-      for(i=1; i<W; i=i+1) begin: g_loop
-         assign filter1[i] = unencoded_i[i] | filter1[i-1];
-         assign filter2[i] = filter1[i] & ~filter1[i-1];
+      if (PRIO == "LOW") begin : gen_low_prio
+         always @* begin
+            encoded_o = {$clog2(WIDTH) {1'd0}};  //In case input is 0
+            for (pos = WIDTH - 1; pos != -1; pos = pos - 1) begin
+               if (unencoded_i[pos]) begin
+                  encoded_o = pos;
+               end
+            end
+         end
+      end else begin : gen_highest_prio  //PRIO == "HIGH"
+         always @* begin
+            encoded_o = {$clog2(WIDTH) {1'd0}};  //In case input is 0
+            for (pos = 0; pos != WIDTH; pos = pos + 1) begin
+               if (unencoded_i[pos]) begin
+                  encoded_o = pos;
+               end
+            end
+         end
       end
    endgenerate
-
-   iob_encoder 
-     #(
-       .W(W)
-       ) 
-   iob_encoder_inst 
-     (
-      .unencoded_i(filter2),
-      .encoded_o(encoded_o)
-      );
 
 endmodule
