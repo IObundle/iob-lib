d2219a394c0cf7a33d0fcc091067d089219cc009
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2023-06-21 01:47:47 +0100

    work around verible linter missing -I flag

--- scripts/verilog-lint.sh
+++ scripts/verilog-lint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # run the command below for all files given as command line arguments
 set -e
-verible-verilog-lint  --rules_config $IOB_LIB_PATH/verible-lint.rules $@
+cat $@ > /tmp/combined
+verible-verilog-lint  --rules_config $IOB_LIB_PATH/verible-lint.rules /tmp/combined
+rm /tmp/combined
