c1aa5af5dbc2c8f2cb819287e202dd5ea5f3ce3f
PMiranda <pmiranda.iobundle@gmail.com>
2023-07-07 16:01:26 +0100

    fix(hw): sync fixes related to clkrst snippets - update sync modules using correct clkrst verilog snippets - add missing comma (,) after include in port and portmap

--- hardware/modules/iob_s2f_sync/iob_s2f_sync.py
+++ hardware/modules/iob_s2f_sync/iob_s2f_sync.py
@@ -5,6 +5,8 @@ from iob_module import iob_module
 from setup import setup
 
 from iob_sync import iob_sync
+from iob_clkrst_portmap import iob_clkrst_portmap
+from iob_clkrst_port import iob_clkrst_port
 
 
 class iob_s2f_sync(iob_module):
@@ -20,6 +22,8 @@ class iob_s2f_sync(iob_module):
         # Setup dependencies
 
         iob_sync.setup()
+        iob_clkrst_port.setup()
+        iob_clkrst_portmap.setup()
 
         # Setup flows of this core using LIB setup function
         setup(cls, disable_file_gen=True)
--- hardware/modules/iob_sync/iob_sync.py
+++ hardware/modules/iob_sync/iob_sync.py
@@ -5,6 +5,7 @@ from iob_module import iob_module
 from setup import setup
 
 from iob_reg import iob_reg
+from iob_clkrst_port import iob_clkrst_port
 
 
 class iob_sync(iob_module):
@@ -20,6 +21,7 @@ class iob_sync(iob_module):
         # Setup dependencies
 
         iob_reg.setup()
+        iob_clkrst_port.setup()
 
         # Setup flows of this core using LIB setup function
         setup(cls, disable_file_gen=True)
