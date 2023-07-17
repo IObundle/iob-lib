1fc0d4fbd752df3480d8e61492941ba408f94f35
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2023-07-17 15:48:03 +0100

    Os sinais de entrada agora suportam mais que 1 bit

--- hardware/modules/iob_aoi/hardware/src/iob_aoi.v
+++ hardware/modules/iob_aoi/hardware/src/iob_aoi.v
@@ -1,18 +1,27 @@
 `timescale 1ns / 1ps
 
-module iob_aoi(
-	input a,
-	input b,
-	input c,
-	input d,
-	output y
+module iob_aoi
+#(
+	parameter W = 21
+) (
+	input [W-1:0] a_i,
+	input [W-1:0] b_i,
+	input [W-1:0] c_i,
+	input [W-1:0] d_i,
+	output [W-1:0] y_o
 );
 
-	wire aad, cad;
+	wire [W-1:0] aab_vec;
+	wire [W-1:0] cad_vec;
 
-	assign aad = a & d;
-	assign cad = c & d;
+	genvar i;
 
-	assign y = ~(aad | cad);
+	generate 
+		for (i = 0; i < W; i = i + 1) begin : and_gen
+			assign aab_vec[i] = a_i[i] & b_i[i];
+			assign cad_vec[i] = c_i[i] & d_i[i];
+			addign y_o[i] = ~(aab_vec[i] | cad_vec[i]);
+		end 
+	endgenerate
 
 endmodule
