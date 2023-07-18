ca120076dfe2030a9200d59cef35ff87e5dc8326
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2023-07-18 15:52:26 +0100

    Simple iob_wire class with name and bit_width as atributes

---
+++ scripts/iob_wire.py
@@ -0,0 +1,9 @@
+class iob_wire:
+    """Class for IOB wire"""
+
+    name = ""
+    bit_width = 1
+
+    def __init__(self, name, bit_width):
+        self.name = name
+        self.bit_width = bit_width
