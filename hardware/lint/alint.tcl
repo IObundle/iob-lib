f7de64f2f97a433ec4cc6fcd88af05c8afd2921a
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2023-08-01 02:48:48 +0100

    fix lint process, but there are still errors

--- hardware/lint/alint.tcl
+++ hardware/lint/alint.tcl
@@ -29,8 +29,9 @@ workspace.file.add -destination $PRJ -f $TOP\_files.list
 
 # Open the sdc files for reading
 set sdcfile1 [open "../syn/umc130/$TOP\_dev.sdc" "r"]
-set sdcfile2 [open "../src/$TOP.sdc" "r"]
-set sdcfile3 [open "../syn/$TOP\_tool.sdc" "r"]
+set sdcfile2 [open "../src/$TOP\.sdc" "r"]
+set sdcfile3 [open "../src/$TOP\_$CSR_IF.sdc" "r"]
+set sdcfile4 [open "../syn/$TOP\_tool.sdc" "r"]
 
 # Open the output file for writing
 set outfile [open "merged.sdc" "w"]
@@ -39,16 +40,19 @@ set outfile [open "merged.sdc" "w"]
 set contents1 [read $sdcfile1]
 set contents2 [read $sdcfile2]
 set contents3 [read $sdcfile3]
+set contents4 [read $sdcfile4]
 
 # Write the contents of the sdc files to the output file
 puts $outfile $contents1
 puts $outfile $contents2
 puts $outfile $contents3
+puts $outfile $contents4
 
 # Close the input and output files
 close $sdcfile1
 close $sdcfile2
 close $sdcfile3
+close $sdcfile4
 close $outfile
 
 
--- hardware/lint/spyglass.mk
+++ hardware/lint/spyglass.mk
@@ -9,17 +9,18 @@ LINT_SSH_FLAGS=$(SYNOPSYS_SSH_FLAGS)
 LINT_SCP_FLAGS=$(SYNOPSYS_SCP_FLAGS)
 LINT_SYNC_FLAGS=$(SYNOPSYS_SYNC_FLAGS)
 
+
 run-lint:
 ifeq ($(LINT_SERVER),)
-	echo exit | spyglass -licqueue -shell -project spyglass.prj -goals "lint/lint_rtl"
+	NAME=$(NAME) CSR_IF=$(CSR_IF) spyglass -licqueue -shell -tcl spyglass.tcl 
 else
 	ssh $(LINT_SSH_FLAGS) $(LINT_USER)@$(LINT_SERVER) "if [ ! -d $(REMOTE_BUILD_DIR) ]; then mkdir -p $(REMOTE_BUILD_DIR); fi"
 	rsync -avz --delete --exclude .git $(LINT_SYNC_FLAGS) ../.. $(LINT_USER)@$(LINT_SERVER):$(REMOTE_BUILD_DIR)
 	ssh -t $(LINT_SSH_FLAGS) $(LINT_USER)@$(LINT_SERVER) 'make -C $(REMOTE_LINT_DIR) run LINTER=$(LINTER)'
 	mkdir -p spyglass_reports
-	scp $(LINT_SCP_FLAGS) $(LINT_USER)@$(LINT_SERVER):$(REMOTE_LINT_DIR)/spyglass/consolidated_reports/$(NAME)_lint_lint_rtl/*.rpt spyglass_reports/.
+	scp $(LINT_SCP_FLAGS) $(LINT_USER)@$(LINT_SERVER):$(REMOTE_LINT_DIR)/spyglass/consolidated_reports/$(LINT_TOP)_lint_lint_rtl/*.rpt spyglass_reports/.
 endif
 
 clean-lint:
 	rm -rf $(NAME)_files.list
-	rm -rf spyglass_reports
+	rm -rf spyglass_reports spyglass.prj
