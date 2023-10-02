0c3395048d11de526bc1481e6823cf9963212c46
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-10-02 16:24:19 +0100

    fix(_copy_src): Remove _copy_srcs method override. Update directory structure of modules to follow IObundle standard.

--- hardware/modules/iob_utils/iob_utils.py
+++ hardware/modules/iob_utils/iob_utils.py
@@ -16,28 +16,6 @@ class iob_utils(iob_module):
     flows = "sim"
     setup_dir = os.path.dirname(__file__)
 
-    # Copy sources of this module to the build directory
-    @classmethod
-    def _copy_srcs(cls): #FIXME: Use new copy_srcs func
-        out_dir = cls.get_purpose_dir(cls._setup_purpose[-1])
-        # Copy source to build directory
-        shutil.copyfile(
-            os.path.join(cls.setup_dir, "iob_utils.vh"),
-            os.path.join(cls.build_dir, out_dir, "iob_utils.vh"),
-        )
-
-        # Ensure sources of other purposes are deleted (except software)
-        # Check that latest purpose is hardware
-        if cls._setup_purpose[-1] == "hardware" and len(cls._setup_purpose) > 1:
-            # Purposes that have been setup previously
-            for purpose in [x for x in cls._setup_purpose[:-1] if x != "software"]:
-                # Delete sources for this purpose
-                os.remove(
-                    os.path.join(
-                        cls.build_dir, cls.cls.PURPOSE_DIRS[purpose], "iob_utils.vh"
-                    )
-                )
-
 
 if __name__ == "__main__":
     iob_utils.setup_as_top_module()
