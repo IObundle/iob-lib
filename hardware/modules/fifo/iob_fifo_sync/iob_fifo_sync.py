0801ba399e45f22f41f499407d204a25e7081485
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-10-02 17:31:13 +0100

    fix(iob_fifo_sync): Add ios to iob_fifo_sync

--- hardware/modules/fifo/iob_fifo_sync/iob_fifo_sync.py
+++ hardware/modules/fifo/iob_fifo_sync/iob_fifo_sync.py
@@ -26,14 +26,160 @@ class iob_fifo_sync(iob_module):
     def _init_attributes(cls):
         """Init module attributes"""
         cls.submodules = [
-            {"interface": "clk_en_rst_s_s_portmap"},
-            {"interface": "clk_en_rst_s_port"},
             iob_reg_r,
             iob_reg,
             iob_counter,
             iob_asym_converter,
             iob_utils,
             (iob_ram_2p, {"purpose": "simulation"}),
+        ]
+
+        cls.ios += [
+            {
+                "name": "clk_en_rst",
+                "type": "slave",
+                "port_prefix": "",
+                "wire_prefix": "",
+                "descr": "Clock, clock enable and reset",
+                "ports": [],
+            },
+            {
+                "name": "rst",
+                "type": "master",
+                "port_prefix": "",
+                "wire_prefix": "",
+                "descr": "Synchronous reset interface",
+                "ports": [
+                    {
+                        "name": "rst_i",
+                        "direction": "input",
+                        "width": 1,
+                        "descr": "Synchronous reset input",
+                    },
+                ],
+            },
+            {
+                "name": "write",
+                "type": "master",
+                "port_prefix": "",
+                "wire_prefix": "",
+                "descr": "Write interface",
+                "ports": [
+                    {
+                        "name": "w_en_i",
+                        "direction": "input",
+                        "width": 1,
+                        "descr": "Write enable",
+                    },
+                    {
+                        "name": "w_data_i",
+                        "direction": "input",
+                        "width": "W_DATA_W",
+                        "descr": "Write data",
+                    },
+                    {
+                        "name": "w_full_o",
+                        "direction": "output",
+                        "width": 1,
+                        "descr": "Write full signal",
+                    },
+                ],
+            },
+            {
+                "name": "read",
+                "type": "master",
+                "port_prefix": "",
+                "wire_prefix": "",
+                "descr": "Read interface",
+                "ports": [
+                    {
+                        "name": "r_en_i",
+                        "direction": "input",
+                        "width": 1,
+                        "descr": "Read enable",
+                    },
+                    {
+                        "name": "r_data_o",
+                        "direction": "output",
+                        "width": "R_DATA_W",
+                        "descr": "Read data",
+                    },
+                    {
+                        "name": "r_empty_o",
+                        "direction": "output",
+                        "width": 1,
+                        "descr": "Read empty signal",
+                    },
+                ],
+            },
+            {
+                "name": "extmem",
+                "type": "master",
+                "port_prefix": "",
+                "wire_prefix": "",
+                "descr": "External memory interface",
+                "ports": [
+                    {
+                        "name": "ext_mem_clk_o",
+                        "direction": "output",
+                        "width": 1,
+                        "descr": "Memory clock",
+                    },
+                    #  Write port
+                    {
+                        "name": "ext_mem_w_en_o",
+                        "direction": "output",
+                        "width": "R",
+                        "descr": "Memory write enable",
+                    },
+                    {
+                        "name": "ext_mem_w_addr_o",
+                        "direction": "output",
+                        "width": "MINADDR_W",
+                        "descr": "Memory write address",
+                    },
+                    {
+                        "name": "ext_mem_w_data_o",
+                        "direction": "output",
+                        "width": "MAXADDR_W",
+                        "descr": "Memory write data",
+                    },
+                    #  Read port
+                    {
+                        "name": "ext_mem_r_en_o",
+                        "direction": "output",
+                        "width": "R",
+                        "descr": "Memory read enable",
+                    },
+                    {
+                        "name": "ext_mem_r_addr_o",
+                        "direction": "output",
+                        "width": "MINADDR_W",
+                        "descr": "Memory read address",
+                    },
+                    {
+                        "name": "ext_mem_r_data_i",
+                        "direction": "input",
+                        "width": "MAXADDR_W",
+                        "descr": "Memory read data",
+                    },
+                ],
+            },
+            {
+                "name": "fifo",
+                "type": "master",
+                "port_prefix": "",
+                "wire_prefix": "",
+                "descr": "FIFO interface",
+                "ports": [
+                    {
+                        "name": "level_o",
+                        "direction": "output",
+                        "width": "ADDR_W+1",
+                        "descr": "FIFO level",
+                    },
+                ],
+            },
         ]
 
 
