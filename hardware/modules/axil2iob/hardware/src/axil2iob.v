8bae59801c57c2f04aa013f5f1ac377c2740b070
PMiranda <pmiranda.iobundle@gmail.com>
2023-10-09 18:42:57 +0100

    fix(axil): fixes to iob2axil and axil2iob modules - update iob2axil and axil2iob modules to work with latest if_gen.py - axil2iob:     - use iob_reg_e (rst is always 0)     - store read address until next address valid operation - iob2axil: use correct hardware src structure

--- hardware/modules/axil2iob/hardware/src/axil2iob.v
+++ hardware/modules/axil2iob/hardware/src/axil2iob.v
@@ -7,10 +7,10 @@ module axil2iob #(
     parameter DATA_W      = AXIL_DATA_W   // IOb data bus width in bits
 ) (
     // Global signals
-    `include "clk_rst_s_port.vs"
+    `include "clk_en_rst_s_port.vs"
 
     // AXI4 Lite slave interface
-    `include "iob_axil_s_port.vs"
+    `include "axil_s_port.vs"
 
     // IOb master interface
     `include "iob_m_port.vs"
@@ -22,7 +22,9 @@ module axil2iob #(
   wire                  iob_rvalid_q;
   wire                  iob_rvalid_e;
   wire                  write_enable;
-  wire [ADDR_WIDTH-1:0] axil_awaddr_q;
+  wire [ADDR_W-1:0]     addr_q;
+  wire                  addr_e;
+  wire [ADDR_W-1:0]     addr_d;
   wire                  axil_bvalid_n;
   wire                  axil_bvalid_e;
 
@@ -30,6 +32,9 @@ module axil2iob #(
   assign axil_bvalid_n = axil_wvalid_i;
   assign axil_bvalid_e = axil_bvalid_n | axil_bready_i;
   assign iob_rvalid_e  = iob_rvalid_i | axil_rready_i;
+
+  assign addr_e = axil_arvalid_i | axil_awvalid_i;
+  assign addr_d = axil_arvalid_i ? axil_araddr_i : axil_awaddr_i;
 
   // COMPUTE AXIL OUTPUTS
   // // write address
@@ -43,48 +48,46 @@ module axil2iob #(
   // // read
   assign axil_rdata_o    = iob_rdata_i;
   assign axil_rresp_o    = 2'b0;
-  assign axil_rvalid_o   = iob_rvalid_i ? 1'b1 : iob_rvalid_q;
+  assign axil_rvalid_o   = iob_rvalid_i;
 
   // COMPUTE IOb OUTPUTS
   assign iob_avalid_o  = (axil_bvalid_n & write_enable) | axil_arvalid_i;
-  assign iob_addr_o    = axil_arvalid_i ? axil_araddr_i : (axil_awvalid_i ? axil_awaddr_i : axil_awaddr_q);
+  assign iob_addr_o    = axil_arvalid_i ? axil_araddr_i : (axil_awvalid_i ? axil_awaddr_i : addr_q);
   assign iob_wdata_o   = axil_wdata_i;
-  assign iob_wstrb_o   = axil_arvalid_i ? {STRB_WIDTH{1'b0}} : axil_wstrb_i;
+  assign iob_wstrb_o   = axil_arvalid_i ? {(DATA_W/8){1'b0}} : axil_wstrb_i;
 
-  iob_reg_re #(
-      .DATA_W (ADDR_WIDTH),
+  // store last read or write address
+  iob_reg_e #(
+      .DATA_W (ADDR_W),
       .RST_VAL(0)
-  ) iob_reg_awaddr (
+  ) iob_reg_addr (
       .clk_i (clk_i),
       .arst_i(arst_i),
       .cke_i (cke_i),
-      .rst_i (1'b0),
-      .en_i  (axil_awvalid_i),
-      .data_i(axil_awaddr_i),
-      .data_o(axil_awaddr_q)
+      .en_i  (addr_e),
+      .data_i(addr_d),
+      .data_o(addr_q)
   );
 
-  iob_reg_re #(
+  iob_reg_e #(
       .DATA_W (1),
       .RST_VAL(0)
   ) iob_reg_rvalid (
       .clk_i (clk_i),
       .arst_i(arst_i),
       .cke_i (cke_i),
-      .rst_i (1'b0),
       .en_i  (iob_rvalid_e),
       .data_i(iob_rvalid_i),
       .data_o(iob_rvalid_q)
   );
 
-  iob_reg_re #(
+  iob_reg_e #(
       .DATA_W (1),
       .RST_VAL(0)
   ) iob_reg_bvalid (
       .clk_i (clk_i),
       .arst_i(arst_i),
       .cke_i (cke_i),
-      .rst_i (1'b0),
       .en_i  (axil_bvalid_e),
       .data_i(axil_bvalid_n),
       .data_o(axil_bvalid_o)
