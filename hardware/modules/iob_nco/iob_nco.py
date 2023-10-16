314320f9e6ef073dc127423e691b6cc9947fa6b8
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-10-16 17:28:21 +0100

    fix(iob_nco): Update iob_nco module

--- hardware/modules/iob_nco/iob_nco.py
+++ hardware/modules/iob_nco/iob_nco.py
@@ -1,33 +1,40 @@
 import os
 
+# Find python modules
+if __name__ == "__main__":
+    import sys
+
+    sys.path.append("./scripts")
 from iob_module import iob_module
+
+if __name__ == "__main__":
+    iob_module.find_modules()
+
 
 from iob_reg_r import iob_reg_r
 from iob_reg import iob_reg
 from iob_modcnt import iob_modcnt
-from iob_ram_2p import iob_ram_2p
 from iob_acc_ld import iob_acc_ld
 from iob_utils import iob_utils
 
 
 class iob_nco(iob_module):
-    name = "iob_nco"
-    version = "V0.10"
-    flows = "sim"
-    setup_dir = os.path.dirname(__file__)
-
     @classmethod
-    def _create_submodules_list(cls):
-        """Create submodules list with dependencies of this module"""
-        super()._create_submodules_list(
-            [
-                {"interface": "clk_en_rst_s_portmap"},
-                {"interface": "clk_en_rst_s_s_portmap"},
-                {"interface": "clk_en_rst_s_port"},
-                iob_reg_r,
-                iob_reg,
-                iob_modcnt,
-                iob_acc_ld,
-                iob_utils,
-            ]
-        )
+    def _init_attributes(cls):
+        """Init module attributes"""
+        cls.name = "iob_nco"
+        cls.version = "V0.10"
+        cls.flows = "sim"
+        cls.setup_dir = os.path.dirname(__file__)
+        cls.submodules = [
+            {"interface": "clk_en_rst"},
+            iob_reg_r,
+            iob_reg,
+            iob_modcnt,
+            iob_acc_ld,
+            iob_utils,
+        ]
+
+
+if __name__ == "__main__":
+    iob_reg.setup_as_top_module()
