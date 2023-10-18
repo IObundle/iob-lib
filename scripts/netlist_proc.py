dce4f6806cece56429142629d4fe8e2a36e567c8
PMiranda <pmiranda.iobundle@gmail.com>
2023-10-18 09:59:17 +0100

    feat(python): improvements to netlist processing - add optional argument for custom prefix. `_` by default. This enables   usage of different prefixes for each netlist to avoid colisions   between netlists - cleanup prints from script

--- scripts/netlist_proc.py
+++ scripts/netlist_proc.py
@@ -5,8 +5,12 @@ import re
 import sys
 
 
-def process_netlist(input, output, top_module):
-    print(f"Processing {input} to {output}")
+def process_netlist(input, output, top_module, prefix="_"):
+    print("Netlist processing:")
+    print(f"\t\tInput: {input}")
+    print(f"\t\tOutput: {output}")
+    print(f"\t\tTop module: {top_module}")
+    print(f"\t\tPrefix: {prefix}")
 
     # search pattern: "module <name>"
     pattern = r"module\s+(\w+)"
@@ -26,10 +30,9 @@ def process_netlist(input, output, top_module):
     # Remove top module
     netlist_modules = [line for line in netlist_modules if line != top_module]
 
-    # replace all instances of <module name> with _<module_name>
+    # replace all instances of <module name> with {prefix}<module_name>
     for module_name in netlist_modules:
-        print(f"Replacing {module_name} with _{module_name}", file=sys.stderr)
-        replacement_string = f"_{module_name}"
+        replacement_string = f"{prefix}{module_name}"
         content = content.replace(module_name, replacement_string)
 
     # write processed netlist
@@ -42,12 +45,10 @@ def parse_args():
     parser.add_argument("input", help="Input netlist file.")
     parser.add_argument("output", help="Output netlist file.")
     parser.add_argument("-t", help="Netlist top level module.")
+    parser.add_argument("-p", default="_", help="Prefix to add to module names.")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
-    print("Netlist Processing Script")
     args = parse_args()
-    print(args)
-    process_netlist(args.input, args.output, args.t)
-    pass
+    process_netlist(args.input, args.output, args.t, args.p)
